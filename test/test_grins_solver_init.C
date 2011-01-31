9cb74b417f0143640582ea83f438038f0c33de7c
Paul T. Bauman <ptbauman@gmail.com>
2011-01-31 19:13:16 +0000

    [GRINS]: Make check works now.

--- test/test_grins_solver_init.C
+++ test/test_grins_solver_init.C
@@ -1,11 +1,12 @@
 #include "grins_solver.h"
+#include "low_mach_num_navier_stokes_sys.h"
 
 int main(int argc, char* argv[])
 {
 
   std::string application_options; // TODO: ADD OPTIONS LATER
 
-  GRINS::Solver grins_solver(application_options);
+  GRINS::Solver<GRINS::LowMachNumberNavierStokesSystem> grins_solver(application_options);
 
   return 0;
 }
