7eccce80e2902c58a528151a7948dd1ddfe1e98b
Paul T. Bauman <ptbauman@gmail.com>
2011-10-20 15:34:51 +0000

    [GRINS]: Adding threading option for thermally driven 3d flow test.

--- test/test_thermally_driven_3d_flow.sh
+++ test/test_thermally_driven_3d_flow.sh
@@ -6,6 +6,8 @@ INPUT="./input_files/thermally_driven_3d_flow.in"
 
 PETSC_OPTIONS="-pc_type ilu" 
 
+LIBMESH_OPTIONS="--n_threads=6"
+
 #-pc_factor_mat_solver_package mumps"
 
-$PROG $INPUT $PETSC_OPTIONS
+$PROG $INPUT $PETSC_OPTIONS $LIBMESH_OPTIONS
