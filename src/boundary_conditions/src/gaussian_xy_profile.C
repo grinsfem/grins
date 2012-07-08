ea4cb114266579fc3744470c692666578bb372c6
Paul T. Bauman <ptbauman@gmail.com>
2012-07-08 23:44:03 +0000

    [GRINS]: Added GaussianProfile and GaussianXYProfile (#2426). Also added unit test for it.

---
+++ src/boundary_conditions/src/gaussian_xy_profile.C
@@ -0,0 +1,73 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2010-2012 The PECOS Development Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2 GNU General
+// Public License as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+//
+//-----------------------------------------------------------------------el-
+//
+// $Id$
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+#include "gaussian_xy_profile.h"
+
+GRINS::GaussianXYProfile::GaussianXYProfile( const double a, const double mu, const double sigma,
+					     const double b )
+  : GaussianProfile(a,mu,sigma,b)
+{
+  _initialized = true;
+  return;
+}
+
+GRINS::GaussianXYProfile::~GaussianXYProfile()
+{
+  return;
+}
+
+libMesh::AutoPtr< libMesh::FunctionBase<libMesh::Number> > GRINS::GaussianXYProfile::clone() const
+{
+  return libMesh::AutoPtr< libMesh::FunctionBase<libMesh::Number> >( new GaussianXYProfile( _a, _mu, std::sqrt(_variance), _b ) );
+}
+
+libMesh::Number GRINS::GaussianXYProfile::operator()( const Point &p, 
+						      const Real )
+{
+  const double r = std::sqrt( p(0)*p(0) + p(1)*p(1) );
+  
+  return this->eval( r );
+}
+
+void GRINS::GaussianXYProfile::operator()( const Point &p, 
+					   const Real time, 
+					   libMesh::DenseVector<libMesh::Number> &output )
+{
+  for( unsigned int i = 0; i < output.size(); i++ )
+    {
+      output(i) = (*this)(p, time);
+    }
+  return;
+}
+
+libMesh::Number GRINS::GaussianXYProfile::operator()( unsigned int i,
+						     const Point &p, 
+						     const Real time )
+{
+  return (*this)(p, time);
+}
---
+++ test/gaussian_profiles.C
@@ -0,0 +1,66 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2010-2012 The PECOS Development Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2 GNU General
+// Public License as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+//
+//-----------------------------------------------------------------------el-
+//
+// $Id$
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+// GRINS
+#include "gaussian_xy_profile.h"
+
+int main() 
+{
+  const double a = 5.0;
+  const double mu = 1.5;
+  const double sigma = 2.1;
+  const double b = 3.2;
+  GRINS::GaussianXYProfile profile( a, mu, sigma, b );
+  
+  const double x = 1.1;
+  const double y = 1.5;
+
+  libMesh::Point p( x, y );
+
+  const double value = profile( p, 0.0 );
+
+  const double r = std::sqrt( x*x + y*y);
+  const double exact_value = a*std::exp( -(r-mu)*(r-mu)/(2.0*sigma*sigma) ) - b;
+
+  const double error = std::fabs( value - exact_value);
+  const double tol = 1.0e-15;
+
+  int return_flag = 0;
+
+  if( error > tol )
+    {
+      std::cout << "Error: GaussianXYProfile tolerance exceeded." << std::endl
+		<< "exact value = " << exact_value << std::endl
+		<< "value = " << value << std::endl
+		<< "error = " << error << std::endl
+		<< "tolerance = " << tol << std::endl;
+      return_flag = 1;
+    }
+
+  return return_flag;
+}
