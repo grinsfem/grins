771c9d4e68ff9c101b20f79135000abdf11717d4
Paul T. Bauman <ptbauman@gmail.com>
2012-07-08 22:02:05 +0000

    [GRINS]: Mumps has started barfing on this example, but SuperLU does OK as well as the new settings. Weird. Probably time to be using newer versions of PETSc.

--- test/test_axi_ns_con_cyl_flow.sh
+++ test/test_axi_ns_con_cyl_flow.sh
@@ -4,8 +4,10 @@ PROG="./test_axi_ns_con_cyl_flow"
 
 INPUT="./input_files/axi_con_cyl_flow.in"
 
-PETSC_OPTIONS="-ksp_type preonly -pc_type lu -pc_factor_mat_solver_package mumps"
-#PETSC_OPTIONS="-pc_type ilu -pc_factor_levels 20"
+#PETSC_OPTIONS="-ksp_type preonly -pc_type lu -pc_factor_mat_solver_package mumps"
+#PETSC_OPTIONS="-ksp_type preonly -pc_type lu -pc_factor_mat_solver_package superlu"
+PETSC_OPTIONS="-pc_type ilu -pc_factor_levels 20"
+#PETSC_OPTIONS="-pc_type bjacobi -sub_pc_type ilu -sub_pc_factor_levels 20"
 #PETSC_OPTIONS="-pc_type lu -pc_factor_shift_type NONZERO"
 
 $PROG $INPUT $PETSC_OPTIONS 
