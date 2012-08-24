9a75afaa7c76f699256716502b39681446405165
Paul T. Bauman <ptbauman@gmail.com>
2012-08-24 20:07:21 +0000

    [GRINS]: Turn off using MUMPS on make check.

--- test/test_axi_ns_poiseuille_flow.sh
+++ test/test_axi_ns_poiseuille_flow.sh
@@ -4,6 +4,7 @@ PROG="./test_axi_ns_poiseuille_flow"
 
 INPUT="./input_files/axi_poiseuille_flow_input.in"
 
-PETSC_OPTIONS="-ksp_type preonly -pc_type lu -pc_factor_mat_solver_package mumps"
+#PETSC_OPTIONS="-ksp_type preonly -pc_type lu -pc_factor_mat_solver_package mumps"
+PETSC_OPTIONS="-ksp_type gmres -pc_type ilu -pc_factor_levels 4"
 
 $PROG $INPUT $PETSC_OPTIONS 
