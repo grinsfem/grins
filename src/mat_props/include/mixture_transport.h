6950e34a7fcecd4187b46f5164e4526824b68e71
Paul T. Bauman <ptbauman@gmail.com>
2012-11-09 15:03:54 +0000

    [GRINS]: Checkpointing some transport work I did last night (#2600).

---
+++ src/mat_props/include/mixture_transport.h
@@ -0,0 +1,67 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2010-2012 The PECOS Development Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2 GNU General
+// Public License as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+//
+//-----------------------------------------------------------------------el-
+//
+// $Id$
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+#ifndef GRINS_MIXTURE_TRANSPORT_H
+#define GRINS_MIXTURE_TRANSPORT_H
+
+// libMesh
+#include "getpot.h"
+#include "libmesh_common.h"
+
+// GRINS
+#include "chemical_mixture.h"
+#include "reacting_flow_cache.h"
+
+namespace GRINS
+{
+  template<typename Viscosity, typename Conductivity, typename Diffusivity>
+  class MixtureTransport
+  {
+  public:
+
+    MixtureTransport( const GetPot& input, const ChemicalMixture& chem_mixture );
+    ~MixtureTransport();
+
+    Real mu( Real T, unsigned int species );
+    Real mu( Real T );
+
+  protected:
+    
+    const ChemicalMixture& _chem_mixture;
+
+    Viscosity _viscosity;
+
+    Conductivity _thermal_conductivity;
+
+    Diffusivity _diffusivity;
+
+  };
+
+} // namespace GRINS
+
+#endif //GRINS_MIXTURE_TRANSPORT_H
