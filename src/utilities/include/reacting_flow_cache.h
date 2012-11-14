42e09e1ddab49a92b07fb4eb563aa6a00d29abd9
Paul T. Bauman <ptbauman@gmail.com>
2012-11-14 18:28:07 +0000

    [GRINS]: Updating ReactingFlowCache to cache quantities we need for reacting low Mach Navier Stokes (#2619).

--- src/utilities/include/reacting_flow_cache.h
+++ src/utilities/include/reacting_flow_cache.h
@@ -34,6 +34,7 @@
 
 // libMesh
 #include "libmesh_common.h"
+#include "vector_value.h"
 
 namespace GRINS
 {
@@ -44,20 +45,111 @@ namespace GRINS
     ReactingFlowCache( Real T, Real P, std::vector<Real>& mass_fractions );
     ~ReactingFlowCache();
 
+    inline
     Real T() const
     { return _T; };
 
+    inline
+    const libMesh::Gradient& grad_T() const
+    { libmesh_assert(_grad_T_set); return _grad_T; }
+
+    inline
     Real P() const
     { return _P; }
 
+    inline
+    Real p_hydro() const
+    { libmesh_assert(_p_hydro_set); return _p_hydro;}
+
+    inline
     const std::vector<Real>& mass_fractions() const
     { return _mass_fractions; }
+
+    inline
+    Real rho() const
+    { libmesh_assert(_chem_props_set); return _rho; }
+
+    inline
+    Real cp() const
+    { libmesh_assert(_thermo_props_set); return _cp; }
+
+    inline
+    Real k() const
+    { libmesh_assert(_trans_props_set); return _k; }
+
+    inline
+    Real mu() const
+    { libmesh_assert(_trans_props_set); return _mu; }
+
+    inline
+    const std::vector<Real>& D() const
+    { libmesh_assert(_trans_props_set); return _D;}
+
+    inline
+    const libMesh::NumberVectorValue& U() const
+    { libmesh_assert(_vel_set); return _U; }
+
+    inline
+    Real divU() const
+    { libmesh_assert(_vel_grad_set); return _divU; }
+
+    inline
+    const libMesh::Gradient& grad_u() const
+    { libmesh_assert(_vel_grad_set); return _grad_u; }
+
+    inline
+    const libMesh::Gradient& grad_v() const
+    { libmesh_assert(_vel_grad_set); return _grad_v; }
+
+    inline
+    const libMesh::Gradient& grad_w() const
+    { libmesh_assert(_vel_grad_set); return _grad_w; }
+
+    void set_chemistry_props( Real R, Real M );
+    void set_thermo_props( Real cp, std::vector<Real>& h );
+    void set_transport_props( Real mu, Real k, std::vector<Real>& D );
     
+    void set_velocities( Real u, Real v, Real w = 0.0 );
+    void set_velocity_grads( libMesh::Gradient& grad_u, libMesh::Gradient& grad_v,
+			     libMesh::Gradient& grad_w );
+
+    void set_p_hyrdo( Real p_hydro );
+
+    void set_temp_grad( libMesh::Gradient& grad_T );
+
   protected:
 
     const Real _T;
     const Real _P;
     const std::vector<Real> _mass_fractions;
+
+    Real _rho;
+
+    Real _R;
+    Real _M;
+    Real _cp;
+    std::vector<Real> _h;
+    Real _mu;
+    Real _k;
+    std::vector<Real> _D;
+
+    libMesh::NumberVectorValue _U;
+    libMesh::Gradient _grad_u;
+    libMesh::Gradient _grad_v;
+    libMesh::Gradient _grad_w;
+    libMesh::Number _divU;
+
+    Real _p_hydro;
+
+    libMesh::Gradient _grad_T;
+
+    bool _chem_props_set;
+    bool _thermo_props_set;
+    bool _trans_props_set;
+    bool _vel_set;
+    bool _vel_grad_set;
+    bool _p_hydro_set;
+    bool _grad_T_set;
 
   private:
     
--- src/utilities/src/reacting_flow_cache.C
+++ src/utilities/src/reacting_flow_cache.C
@@ -34,7 +34,14 @@ namespace GRINS
   ReactingFlowCache::ReactingFlowCache( Real T, Real P, std::vector<Real>& mass_fractions )
     : _T(T),
       _P(P),
-      _mass_fractions(mass_fractions)
+      _mass_fractions(mass_fractions),
+      _chem_props_set(false),
+      _thermo_props_set(false),
+      _trans_props_set(false),
+      _vel_set(false),
+      _vel_grad_set(false),
+      _p_hydro_set(false),
+      _grad_T_set(false)
   {
     return;
   }
@@ -44,4 +51,61 @@ namespace GRINS
     return;
   }
 
+  void ReactingFlowCache::set_chemistry_props( Real R, Real M )
+  {
+    _R = R;
+    _M = M;
+    _rho = _P/(_R*_T);
+    _chem_props_set = true;
+    return;
+  }
+
+  void ReactingFlowCache::set_thermo_props( Real cp, std::vector<Real>& h )
+  {
+    _cp = cp;
+    _h = h;
+    _thermo_props_set = true;
+    return;
+  }
+
+  void ReactingFlowCache::set_transport_props( Real mu, Real k, std::vector<Real>& D )
+  {
+    _mu = mu;
+    _k = k;
+    _D = D;
+    _trans_props_set = true;
+    return;
+  }
+
+  void ReactingFlowCache::set_velocities( Real u, Real v, Real w )
+  {
+    _U = libMesh::NumberVectorValue( u, v, w );
+    _vel_set = true;
+    return;
+  }
+
+  void ReactingFlowCache::set_velocity_grads( libMesh::Gradient& grad_u, libMesh::Gradient& grad_v,
+					      libMesh::Gradient& grad_w )
+  {
+    _grad_u = grad_u;
+    _grad_v = grad_v;
+    _grad_w = grad_w;
+    _divU = grad_u(0) + grad_v(1) + grad_w(2);
+    _vel_grad_set = true;
+    return;
+  }
+
+  void ReactingFlowCache::set_p_hyrdo( Real p_hydro )
+  {
+    _p_hydro = p_hydro;
+    _p_hydro_set = true;
+    return;
+  }
+
+  void ReactingFlowCache::set_temp_grad( libMesh::Gradient& grad_T )
+  {
+    _grad_T = grad_T;
+    _grad_T_set = true;
+    return;
+  }
 }
