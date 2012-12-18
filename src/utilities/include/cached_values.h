e8c87ea7bec4ba502e8f4eac1939c4aec8cf61ea
Paul T. Bauman <ptbauman@gmail.com>
2012-12-18 22:04:50 +0000

    [GRINS]: Initial draft of CachedValues class (#2650). Not being used yet.

---
+++ src/utilities/include/cached_values.h
@@ -0,0 +1,110 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2010-2012 The PECOS Development Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2 GNU General
+// Public License as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+//
+//-----------------------------------------------------------------------el-
+//
+// $Id$
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+#ifndef GRINS_CACHED_VALUES_H
+#define GRINS_CACHED_VALUES_H
+
+//C++
+#include <set>
+#include <vector>
+#include <map>
+
+// libMesh
+#include "libmesh.h"
+
+namespace GRINS
+{
+  class CachedValues
+  {
+  public:
+
+    CachedValues();
+    ~CachedValues();
+
+    enum CachedQuantities{ X_VELOCITY = 0,
+			   Y_VELOCITY,
+			   Z_VELOCITY,
+			   X_VELOCITY_GRAD,
+			   Y_VELOCITY_GRAD,
+			   Z_VELOCITY_GRAD,
+			   PRESSURE,
+			   THERMO_PRESSURE,
+			   TEMPERATURE,
+			   TEMPERATURE_GRAD,
+			   PERFECT_GAS_DENSITY,
+			   MIXTURE_DENSITY,
+			   PERFECT_GAS_VISCOSITY,
+			   SPECIES_VISCOSITY,
+			   MIXTURE_VISCOSITY,
+			   PERFECT_GAS_THERMAL_CONDUCTIVITY,
+			   SPECIES_THERMAL_CONDUCTIVITY,
+			   MIXTURE_THERMAL_CONDUCTIVITY,
+			   PERFECT_GAS_SPECIFIC_HEAT_P,
+			   SPECIES_SPECIFIC_HEAT_P,
+			   MIXTURE_SPECIFIC_HEAT_P,
+			   PERFECT_GAS_SPECIFIC_HEAT_V,
+			   SPECIES_SPECIFIC_HEAT_V,
+			   MIXTURE_SPECIFIC_HEAT_V,
+			   MASS_FRACTIONS,
+			   MASS_FRACTION_GRADIENTS,
+			   MOLE_FRACTIONS,
+			   MOLAR_MASS,
+			   SPECIES_GAS_CONSTANTS,
+			   MIXTURE_GAS_CONSTANT,
+			   DIFFUSION_COEFFS,
+			   SPECIES_ENTHALPY,
+			   OMEGA_DOT };
+
+    void add_quantity( CachedQuantities quantity );
+
+    void add_quantities( const std::set<CachedQuantities>& cache_list );
+
+    void clear();
+
+    bool is_active(CachedQuantities quantity);
+
+    void set_values( CachedQuantities quantity, std::vector<Number>& values );
+
+    void set_vector_values( CachedQuantities quantity, std::vector<std::vector<Number> >& values );
+
+    const std::vector<Number>& get_cached_values( CachedQuantities quantity ) const;
+
+    const std::vector<std::vector<Number> >& get_cached_vector_values( CachedQuantities quantity ) const;
+
+  protected:
+    
+    std::set<CachedQuantities> _cache_list;
+
+    std::map<CachedQuantities,std::vector<Number> > _cached_values;
+    std::map<CachedQuantities,std::vector<std::vector<Number> >  > _cached_vector_values;
+    
+  };
+
+} // namespace GRINS
+
+#endif // GRINS_CACHED_VALUES_H
---
+++ src/utilities/src/cached_values.C
@@ -0,0 +1,96 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2010-2012 The PECOS Development Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2 GNU General
+// Public License as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+//
+//-----------------------------------------------------------------------el-
+//
+// $Id$
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+#include "cached_values.h"
+
+namespace GRINS
+{
+  CachedValues::CachedValues()
+  {
+    return;
+  }
+
+  CachedValues::~CachedValues()
+  {
+    return;
+  }
+
+  void CachedValues::add_quantity( CachedQuantities quantity )
+  {
+    _cache_list.insert(quantity);
+    return;
+  }
+
+  void CachedValues::add_quantities( const std::set<CachedQuantities>& cache_list )
+  {
+    _cache_list.insert(cache_list.begin(), cache_list.end());
+    return;
+  }
+
+  void CachedValues::clear()
+  {
+    _cached_values.clear();
+    _cached_vector_values.clear();
+    return;
+  }
+
+  bool CachedValues::is_active(CachedQuantities quantity)
+  {
+    bool value = false;
+
+    if( _cache_list.find(quantity) != _cache_list.end() )
+      value = true;
+
+    return value;
+  }
+
+  void CachedValues::set_values( CachedQuantities quantity, std::vector<Number>& values )
+  {
+    _cached_values.insert( std::make_pair( quantity, values ) );
+    return;
+  }
+
+  void CachedValues::set_vector_values( CachedQuantities quantity, std::vector<std::vector<Number> >& values )
+  {
+    _cached_vector_values.insert( std::make_pair( quantity, values ) );
+    return;
+  }
+
+  const std::vector<Number>& CachedValues::get_cached_values( CachedQuantities quantity ) const
+  {
+    libmesh_assert( _cached_values.find(quantity) != _cached_values.end() );
+    return _cached_values.find(quantity)->second;
+  }
+
+  const std::vector<std::vector<Number> >& CachedValues::get_cached_vector_values( CachedQuantities quantity ) const
+  {
+    libmesh_assert( _cached_vector_values.find(quantity) != _cached_vector_values.end() );
+    return _cached_vector_values.find(quantity)->second;
+  }
+
+} // namespace GRINS
