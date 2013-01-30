867a4914167b9f644dcd8326575d311ec5a7e9df
Paul T. Bauman <ptbauman@gmail.com>
2013-01-30 23:20:00 +0000

    [GRINS]: Added first part of CatalyticWall unit test (#2630).

---
+++ test/catalytic_wall_unit.C
@@ -0,0 +1,106 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2010-2012 The PECOS Development Team
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2 GNU General
+// Public License as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+//
+//-----------------------------------------------------------------------el-
+//
+// $Id$
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+// C++
+#include <iomanip>
+
+// GRINS
+#include "grins/chemical_mixture.h"
+#include "grins/catalytic_wall.h"
+
+int main()
+{
+  std::vector<std::string> species_str_list;
+  const unsigned int n_species = 5;
+  species_str_list.reserve(n_species);
+  species_str_list.push_back( "N2" );
+  species_str_list.push_back( "O2" );
+  species_str_list.push_back( "N" );
+  species_str_list.push_back( "O" );
+  species_str_list.push_back( "NO" );
+
+  GRINS::ChemicalMixture chem_mixture( species_str_list );
+
+  const unsigned int N_index = 2;
+
+  const GRINS::VariableIndex T_var_dummy = 5;
+
+  const double gamma = 0.03;
+
+  GRINS::CatalyticWall wall_N( chem_mixture, N_index, T_var_dummy, gamma );
+
+  GRINS::CatalyticWall wall_N2( chem_mixture, N_index, T_var_dummy, -gamma );
+
+  const double rho_s = 1.0e-3;
+
+  const double T = 620.1;
+
+  const double R_N = chem_mixture.R( chem_mixture.active_species_name_map().find("N")->second );
+
+  const double M_N = chem_mixture.M( chem_mixture.active_species_name_map().find("N")->second );
+
+  const double omega_dot_exact = rho_s*gamma*std::sqrt( R_N*T/(GRINS::Constants::two_pi*M_N) );
+
+  int return_flag = 0;
+
+  const double omega_dot_N = wall_N.omega_dot( rho_s, T );
+  const double omega_dot_N2 = wall_N2.omega_dot( rho_s, T );
+
+  const double tol = 1.0e-15;
+
+  {
+    double rel_error = std::fabs( (omega_dot_N - omega_dot_exact)/omega_dot_exact );
+
+    if( rel_error > tol )
+      {
+	std::cerr << "Mismatch in omega_dot_N!" << std::endl
+		  << "omega_dot_N = " << omega_dot_N << std::endl
+		  << "omega_dot_exact = " << omega_dot_exact << std::endl
+		  << "rel error = " << rel_error << std::endl;
+
+	return_flag = 1;
+      }
+  }
+
+  {
+    double rel_error = std::fabs( (omega_dot_N2 + omega_dot_exact)/omega_dot_exact );
+
+    if( rel_error > tol )
+      {
+	std::cerr << "Mismatch in omega_dot_N2!" << std::endl
+		  << "omega_dot_N2    = " << omega_dot_N2 << std::endl
+		  << "omega_dot_exact = " << omega_dot_exact << std::endl
+		  << "rel error = " << rel_error << std::endl;
+
+	return_flag = 1;
+      }
+  }
+    
+  return return_flag;
+}
+
