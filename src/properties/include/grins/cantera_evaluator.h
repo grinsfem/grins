4cf5e711d2ffd4a40ea72d19e8e35d618cd2b19a
Paul T. Bauman <ptbauman@gmail.com>
2013-05-26 07:44:36 +0000

    [GRINS]: Oops, that should be pass by reference.

--- src/properties/include/grins/cantera_evaluator.h
+++ src/properties/include/grins/cantera_evaluator.h
@@ -81,7 +81,7 @@ namespace GRINS
     libMesh::Real k( const CachedValues& cache, unsigned int qp ) const;
 
     void mu_and_k( const CachedValues& cache, unsigned int qp,
-                   libMesh::Real& mu, libMesh::Real k );
+                   libMesh::Real& mu, libMesh::Real& k );
 
     void D( const CachedValues& cache, unsigned int qp,
 	    std::vector<libMesh::Real>& D ) const;
@@ -194,8 +194,9 @@ namespace GRINS
     return _transport.k(cache,qp);
   }
 
+  inline
   void CanteraEvaluator::mu_and_k( const CachedValues& cache, unsigned int qp,
-                                   libMesh::Real& mu, libMesh::Real k )
+                                   libMesh::Real& mu, libMesh::Real& k )
   {
     mu = _transport.mu(cache,qp);
     k = _transport.k(cache,qp);
