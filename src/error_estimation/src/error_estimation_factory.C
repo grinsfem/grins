0ab0d3175b84a3f8902dedd061affc88bae1e6fd
Roy H. Stogner <roystgnr@ices.utexas.edu>
2013-08-05 12:50:52 -0500

    Fix a ton of "make check" breakage
    Every test that didn't specify an error estimator (regardless of
    whether the test was doing error estimation) was failing here.

--- src/error_estimation/src/error_estimation_factory.C
+++ src/error_estimation/src/error_estimation_factory.C
@@ -47,7 +47,7 @@ namespace GRINS
   std::tr1::shared_ptr<libMesh::ErrorEstimator> ErrorEstimatorFactory::build( const GetPot& input, 
                                                                               const libMesh::QoISet& qoi_set )
   {
-    std::string estimator_type = input("MeshAdaptivity/estimator_type", "none");
+    std::string estimator_type = input("MeshAdaptivity/estimator_type", "patch_recovery");
 
     ErrorEstimatorEnum estimator_enum = this->string_to_enum( estimator_type );
 
