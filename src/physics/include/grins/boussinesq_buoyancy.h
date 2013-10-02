6a8e1daa73f25e285c5cc441c0e142c75da526f2
Paul T. Bauman <ptbauman@gmail.com>
2013-10-02 13:46:47 -0500

    Moved common functionality to base class for Boussinesq buoyancy in preparation for stabilization term.

--- src/physics/include/grins/boussinesq_buoyancy.h
+++ src/physics/include/grins/boussinesq_buoyancy.h
@@ -26,12 +26,7 @@
 #define GRINS_BOUSSINESQ_BUOYANCY_H
 
 // GRINS
-#include "grins/physics.h"
-
-// libMesh
-#include "libmesh/enum_order.h"
-#include "libmesh/enum_fe_family.h"
-#include "libmesh/point.h"
+#include "grins/boussinesq_buoyancy_base.h"
 
 namespace GRINS
 {  
@@ -49,16 +44,13 @@ namespace GRINS
     element_time_derivative routine. This class requires a flow physics enabled
     and the ConvectiveHeatTransfer physics class enabled.
    */
-  class BoussinesqBuoyancy : public Physics
+  class BoussinesqBuoyancy : public BoussinesqBuoyancyBase
   {
   public:
     
     BoussinesqBuoyancy( const std::string& physics_name, const GetPot& input );
 
     ~BoussinesqBuoyancy();
-
-    //! Initialization of BoussinesqBuoyancy variables
-    virtual void init_variables( libMesh::FEMSystem* system );
 
     //! Source term contribution for BoussinesqBuoyancy
     /*! This is the main part of the class. This will add the source term to
@@ -67,54 +59,6 @@ namespace GRINS
     virtual void element_time_derivative( bool compute_jacobian,
 					  AssemblyContext& context,
 					  CachedValues& cache );
-
-  protected:
-
-    //! Element type, read from input
-    libMeshEnums::FEFamily _T_FE_family, _V_FE_family;
-
-    //! Temperature element order, read from input
-    libMeshEnums::Order _T_order, _V_order;
-
-    //! Name of x-velocity
-    std::string _u_var_name;
-
-    //! Name of y-velocity
-    std::string _v_var_name;
-
-    //! Name of z-velocity
-    std::string _w_var_name;
-
-    //! Name of temperature
-    std::string _T_var_name;
-
-    //! \f$ \rho_0 = \f$ reference density
-    libMesh::Number _rho_ref;
-
-    //! \f$ T_0 = \f$ reference temperature 
-    libMesh::Number _T_ref;
-
-    //! \f$ \beta_T = \f$ coefficient of thermal expansion
-    libMesh::Number _beta_T;
-
-    //! Gravitational vector
-    libMesh::Point _g;
-
-     //! Physical dimension of problem
-    unsigned int _dim;
-
-    // Indices for each variable;
-    //! Index for x-velocity field
-    VariableIndex _u_var;
-
-    //! Index for y-velocity field
-    VariableIndex _v_var;
-
-    //! Index for z-velocity field
-    VariableIndex _w_var;
-
-    //! Index for temperature field
-    VariableIndex _T_var;
 
   private:
 
