fcfe7656dcf7f829469873cdfe9b75fd72a1c06b
Paul T. Bauman <ptbauman@gmail.com>
2013-10-04 17:05:39 -0500

    First draft of HeatTransfer SPGSM stabilization.

---
+++ src/physics/include/grins/heat_transfer_spgsm_stab.h
@@ -0,0 +1,57 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef GRINS_HEAT_TRANSFER_SPGSM_STAB_H
+#define GRINS_HEAT_TRANSFER_SPGSM_STAB_H
+
+//GRINS
+#include "grins/heat_transfer_stab_base.h"
+
+//! GRINS namespace
+namespace GRINS
+{
+  //! Adds VMS-based stabilization to LowMachNavierStokes physics class
+  class HeatTransferSPGSMStabilization : public HeatTransferStabilizationBase
+  {
+
+  public:
+
+    HeatTransferSPGSMStabilization( const GRINS::PhysicsName& physics_name, const GetPot& input );
+    virtual ~HeatTransferSPGSMStabilization();
+
+    virtual void element_time_derivative( bool compute_jacobian,
+                                          AssemblyContext& context,
+                                          CachedValues& cache );
+
+    virtual void mass_residual( bool compute_jacobian,
+                                AssemblyContext& context,
+                                CachedValues& cache );
+    
+  private:
+    HeatTransferSPGSMStabilization();
+
+  }; // End HeatTransferSPGSMStabilization class declarations
+
+} // End namespace GRINS
+
+#endif // GRINS_HEAT_TRANSFER_SPGSM_STAB_H
