01522ac1fb7a734e56275c44550cfb94b8dc68d9
Paul T. Bauman <ptbauman@gmail.com>
2013-10-16 23:53:26 -0500

    Added Arrhenius type catalycity.

---
+++ src/boundary_conditions/include/grins/arrhenius_catalycity.h
@@ -0,0 +1,60 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef GRINS_ARRHENIUS_CATALYCITY_H
+#define GRINS_ARRHENIUS_CATALYCITY_H
+
+// GRINS
+#include "grins/catalycity_base.h"
+
+namespace GRINS
+{
+  class ArrheniusCatalycity : public CatalycityBase
+  {
+  public:
+
+    ArrheniusCatalycity( const libMesh::Real gamma0, const libMesh::Real Ta );
+
+    virtual ~ArrheniusCatalycity();
+
+    virtual libMesh::Real operator()( const libMesh::Real T ) const;
+
+    virtual libMesh::Real dT( const libMesh::Real T ) const;
+    
+    virtual void set_params( const std::vector<libMesh::Real>& params );
+    
+  protected:
+
+    libMesh::Real _gamma0;
+
+    libMesh::Real _Ta;
+
+  private:
+
+    ArrheniusCatalycity();
+
+  };
+
+} // end namespace GRINS
+
+#endif // GRINS_ARRHENIUS_CATALYCITY_H
