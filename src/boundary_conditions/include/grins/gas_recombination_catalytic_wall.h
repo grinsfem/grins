9d082cde6f7356784fa45dd9f81b20dd77412de0
Paul T. Bauman <ptbauman@gmail.com>
2013-11-18 13:52:11 -0600

    First draft of GasRecombinationCatalyticWall
    This class handles the case of a catalytic wall where a gas is
    recombining, e.g. N+N -> N_2. We are making an assumption that
    only these species are involved in the reaction so that the product
    mass fluxes can be computed as minus the reactant fluxes.

---
+++ src/boundary_conditions/include/grins/gas_recombination_catalytic_wall.h
@@ -0,0 +1,90 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef GRINS_GAS_RECOMBINATION_CATALYTIC_WALL_H
+#define GRINS_GAS_RECOMBINATION_CATALYTIC_WALL_H
+
+namespace GRINS
+{
+
+  template<typename Chemistry>
+  class GasRecombinationCatalyticWall : public CatalyticWallBase<Chemistry>
+  {
+  public:
+
+    GasRecombinationCatalyticWall( const Chemistry& chem_mixture,
+                                   CatalycityBase& gamma,
+                                   const unsigned int reactant_species_idx,
+                                   const unsigned int product_species_idx );
+
+    virtual ~GasRecombinationCatalyticWall();
+
+    virtual void init( const libMesh::FEMSystem& system );
+
+    virtual void apply_fluxes( AssemblyContext& context,
+                               const CachedValues& cache,
+                               const bool request_jacobian );
+
+    libMesh::Real compute_reactant_mass_flux( const libMesh::Real rho,
+                                              const libMesh::Real Y_r,
+                                              const libMesh::Real T );
+
+    libMesh::Real compute_product_mass_flux( const libMesh::Real rho,
+                                             const libMesh::Real Y_r,
+                                             const libMesh::Real T );
+
+  protected:
+
+    const unsigned int _reactant_species_idx;
+    VariableIndex _reactant_var_idx;
+
+    const unsigned int _product_species_idx;
+    VariableIndex _product_var_idx;
+
+  };
+
+  template<typename Chemistry>
+  inline
+  libMesh::Real GasRecombinationCatalyticWall<Chemistry>::compute_reactant_mass_flux( const libMesh::Real rho,
+                                                                                      const libMesh::Real Y_r,
+                                                                                      const libMesh::Real T )
+  {  
+    const libMesh::Real rho_r = rho*Y_r;
+    
+    const libMesh::Real omega_dot = this->omega_dot( rho_r, T );
+
+    return -omega_dot;
+  }
+
+  template<typename Chemistry>
+  inline
+  libMesh::Real GasRecombinationCatalyticWall<Chemistry>::compute_product_mass_flux( const libMesh::Real rho,
+                                                                                     const libMesh::Real Y_r,
+                                                                                     const libMesh::Real T )
+  {
+    return -(this->compute_reactant_mass_flux(rho,Y_r,T));
+  }
+
+} // end namespace GRINS
+
+#endif // GRINS_GAS_RECOMBINATION_CATALYTIC_WALL_H
---
+++ src/boundary_conditions/src/gas_recombination_catalytic_wall.C
@@ -0,0 +1,122 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+// This class
+#include "grins/gas_recombination_catalytic_wall.h"
+
+// libMesh
+#include "libmesh/quadrature.h"
+
+namespace GRINS
+{
+  template<typename Chemistry>
+  GasRecombinationCatalyticWall<Chemistry>::GasRecombinationCatalyticWall( const Chemistry& chem_mixture,
+                                                                           CatalycityBase& gamma,
+                                                                           const unsigned int reactant_species_idx,
+                                                                           const unsigned int product_species_idx )
+    : CatalyticWallBase<Chemistry>(chem_mixture,gamma,reactant_species_idx),
+    _reactant_species_idx(reactant_species_idx),
+    _product_species_idx(product_species_idx)
+  {
+    return;
+  }
+
+  template<typename Chemistry>
+  GasRecombinationCatalyticWall<Chemistry>::~GasRecombinationCatalyticWall()
+  {
+    return;
+  }
+
+  template<typename Chemistry>
+  void GasRecombinationCatalyticWall<Chemistry>::init( const libMesh::FEMSystem& system )
+  {
+    const std::string r_var_name = std::string("w_"+this->_chemistry.species_name( this->_reactant_species_idx ) );
+
+    const std::string p_var_name = std::string("w_"+this->_chemistry.species_name( this->_product_species_idx ) );
+
+    libmesh_assert( system.has_variable( r_var_name ) );
+    libmesh_assert( system.has_variable( p_var_name ) );
+
+    this->_reactant_var_idx = system.variable_number( r_var_name );
+
+    this->_product_var_idx = system.variable_number( p_var_name );
+
+    return;
+  }
+
+  template<typename Chemistry>
+  void GasRecombinationCatalyticWall<Chemistry>::apply_fluxes( AssemblyContext& context,
+                                                               const CachedValues& cache,
+                                                               const bool request_jacobian )
+  {
+    libMesh::FEGenericBase<libMesh::Real>* side_fe = NULL; 
+    context.get_side_fe( _reactant_var_idx, side_fe );
+
+    // The number of local degrees of freedom in each variable.
+    const unsigned int n_var_dofs = context.get_dof_indices(_reactant_var_idx).size();
+
+    libmesh_assert_equal_to( n_var_dofs, context.get_dof_indices(_product_var_idx).size() );
+
+    // Element Jacobian * quadrature weight for side integration.
+    const std::vector<libMesh::Real> &JxW_side = side_fe->get_JxW();
+
+    // The var shape functions at side quadrature points.
+    const std::vector<std::vector<libMesh::Real> >& var_phi_side = side_fe->get_phi();
+
+    // reactant residual
+    libMesh::DenseSubVector<libMesh::Number> &F_r_var = context.get_elem_residual(_reactant_var_idx); 
+
+     // product residual
+    libMesh::DenseSubVector<libMesh::Number> &F_p_var = context.get_elem_residual(_product_var_idx);
+
+    unsigned int n_qpoints = context.get_side_qrule().n_points();
+
+    for (unsigned int qp=0; qp != n_qpoints; qp++)
+      {
+        const libMesh::Real rho = cache.get_cached_values(Cache::MIXTURE_DENSITY)[qp];
+
+        const libMesh::Real Y_r = cache.get_cached_vector_values(Cache::MASS_FRACTIONS)[qp][this->_reactant_species_idx];
+
+        const libMesh::Real T = cache.get_cached_values(Cache::TEMPERATURE)[qp];
+
+        const libMesh::Real r_value = this->compute_reactant_mass_flux(rho, Y_r, T);
+
+        const libMesh::Real p_value = -r_value;
+
+	for (unsigned int i=0; i != n_var_dofs; i++)
+	  {
+	    F_r_var(i) += r_value*var_phi_side[i][qp]*JxW_side[qp];
+
+            F_p_var(i) += p_value*var_phi_side[i][qp]*JxW_side[qp];
+
+            if( request_jacobian )
+              {
+                libmesh_not_implemented();
+              }
+	  }
+      }
+
+    return;
+  }
+
+} // end namespace GRINS
