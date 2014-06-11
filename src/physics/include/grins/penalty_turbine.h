c26314174f1e97ad6ff408a2cedf22311d756e1c
Roy H. Stogner <roystgnr@ices.utexas.edu>
2014-06-11 16:03:57 -0500

    PenaltyTurbine will be similar to VelocityPenalty
    So we'll start with a fork of that.  But the eventual changes are
    going to be big enough (an additional SCALAR variable) to warrant a
    new class IMHO.

---
+++ src/physics/include/grins/penalty_turbine.h
@@ -0,0 +1,79 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+
+#ifndef GRINS_PENALTY_TURBINE_H
+#define GRINS_PENALTY_TURBINE_H
+
+// GRINS
+#include "grins_config.h"
+#include "grins/assembly_context.h"
+#include "grins/cached_values.h"
+#include "grins/inc_navier_stokes_base.h"
+
+// libMesh
+#include "libmesh/fem_system.h"
+#include "libmesh/getpot.h"
+
+// C++
+#include <string>
+
+namespace GRINS
+{
+
+  //! Physics class for Penalty-based Turbine
+  /*
+    This physics class imposes a penalty on any velocity component in
+    the direction of (and proportional to) a specified vector field.
+   */
+  class PenaltyTurbine : public IncompressibleNavierStokesBase
+  {
+  public:
+
+    PenaltyTurbine( const std::string& physics_name, const GetPot& input );
+
+    ~PenaltyTurbine();
+
+    //! Read options from GetPot input file.
+    virtual void read_input_options( const GetPot& input );
+    
+    // residual and jacobian calculations
+    // element_*, side_* as *time_derivative, *constraint, *mass_residual
+
+    // Constraint part(s)
+    virtual void element_constraint( bool compute_jacobian,
+				     AssemblyContext& context,
+				     CachedValues& cache );
+
+  private:
+
+    libMesh::AutoPtr<libMesh::FunctionBase<libMesh::Number> > normal_vector_function;
+
+    libMesh::AutoPtr<libMesh::FunctionBase<libMesh::Number> > base_velocity_function;
+
+    PenaltyTurbine();
+  };
+
+} // end namespace block
+
+#endif // GRINS_PENALTY_TURBINE_H
