a590c4f7bd03b311055294ea6e9d3c50a4f73cf5
Paul T. Bauman <ptbauman@gmail.com>
2014-06-17 23:43:15 -0400

    Updated kinetics regression for Antioch.
    This is because of the slight change for the universal gas constant
    conversion to calories (see discussion in #24).

--- test/antioch_evaluator_regression.C
+++ test/antioch_evaluator_regression.C
@@ -229,11 +229,11 @@ int test_evaluator( const GRINS::AntiochMixture& antioch_mixture )
 
   // Now check against regression values
   std::vector<libMesh::Real> omega_dot_reg(n_species,0.0);
-  omega_dot_reg[0] =  4.3563269373325170e-01;
-  omega_dot_reg[1] = -3.6798048754802180e+00;
-  omega_dot_reg[2] =  2.9971144322942522e+00;
-  omega_dot_reg[3] = -1.8347122381073480e+00;
-  omega_dot_reg[4] =  2.0817699875600622e+00;
+  omega_dot_reg[0] =  4.3561014567820650e-01;
+  omega_dot_reg[1] = -3.6797207156306633e+00;
+  omega_dot_reg[2] =  2.9970582875850726e+00;
+  omega_dot_reg[3] = -1.8346634812051223e+00;
+  omega_dot_reg[4] =  2.0817157635725065e+00;
 
   for( unsigned int s = 0; s < n_species; s++ )
     {
