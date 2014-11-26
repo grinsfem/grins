3843957d842aa6a33484dd2deac857b48d3ee3d3
Vikram Garg <vikram@grignard.ices.utexas.edu>
2014-11-25 20:27:07 -0600

    Created a Spalart Allmaras turbulence physics, and a turbulence variables and FE variables class

---
+++ src/physics/include/grins/spalart_allmaras.h
@@ -0,0 +1,78 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2014 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+
+#ifndef GRINS_SPALART_ALLMARAS_H
+#define GRINS_SPALART_ALLMARAS_H
+
+//GRINS
+#include "grins/physics.h"
+#include "grins/turbulence_fe_variables.h"
+
+namespace GRINS
+{
+
+  //! Physics class for Incompressible Navier-Stokes
+  /*!
+    This physics class implements the classical Incompressible Navier-Stokes equations.
+    This is a templated class, the class Viscosity can be instantiated as a specific type
+    (right now:ConstantViscosity or SpatiallyVaryingViscosity) to allow the user
+    to specify a constant or spatially varying viscosity in the input file
+   */
+  template<class Viscosity>
+  class SpalartAllmaras : public Physics
+  {
+  public:
+
+    SpalartAllmaras(const std::string& physics_name, const GetPot& input);
+
+    ~SpalartAllmaras();
+        
+    virtual void init_variables( libMesh::FEMSystem* system );
+
+    //! Sets velocity variables to be time-evolving
+    virtual void set_time_evolving_vars( libMesh::FEMSystem* system );
+
+    // Context initialization
+    virtual void init_context( AssemblyContext& context );    
+
+  protected:
+
+    //! Physical dimension of problem
+    /*! \todo Do we really need to cache this? */
+    unsigned int _dim;
+
+    TurbulenceFEVariables _turbulence_vars;
+ 
+    //! Viscosity object
+    Viscosity _mu;
+    
+  private:
+    SpalartAllmaras();
+
+  };
+
+} //End namespace block
+
+#endif // GRINS_SPALART_ALLMARAS_H
---
+++ src/physics/include/grins/turbulence_fe_variables.h
@@ -0,0 +1,71 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2014 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef GRINS_TURBULENCE_FE_VARIABLES_H
+#define GRINS_TURBULENCE_FE_VARIABLES_H
+
+// GRINS
+#include "grins/grins_enums.h"
+#include "grins/turbulence_variables.h"
+#include "grins/var_typedefs.h"
+
+//libMesh
+#include "libmesh/enum_order.h"
+#include "libmesh/enum_fe_family.h"
+
+// libMesh forward declarations
+class GetPot;
+namespace libMesh
+{
+  class FEMSystem;
+}
+
+namespace GRINS
+{
+  class TurbulenceFEVariables : public TurbulenceVariables
+  {
+  public:
+
+    TurbulenceFEVariables( const GetPot& input, const std::string& physics_name );
+    ~TurbulenceFEVariables();
+
+    virtual void init( libMesh::FEMSystem* system );
+
+  protected:
+
+    //! Element type, read from input
+    GRINSEnums::FEFamily _TU_FE_family;
+
+    //! Element orders, read from input
+    GRINSEnums::Order _TU_order;
+
+  private:
+
+    TurbulenceFEVariables();
+
+  };
+
+} // end namespace GRINS
+
+#endif // GRINS_TURBULENCE_FE_VARIABLES_H
