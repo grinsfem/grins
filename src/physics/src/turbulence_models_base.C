287b3d0f71c76c3a7f27dd0e48fb9cbf1ec685b3
Vikram V Garg <vikramvg@mit.edu>
2014-12-17 01:07:22 -0600

    Definitions for the TurbulenceModelsBase class functions

---
+++ src/physics/src/turbulence_models_base.C
@@ -0,0 +1,63 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+// 
+// GRINS - General Reacting Incompressible Navier-Stokes 
+//
+// Copyright (C) 2014 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+
+// This class
+#include "grins/turbulence_models_base.h"
+
+// GRINS
+#include "grins/assembly_context.h"
+#include "grins/constant_viscosity.h"
+#include "grins/parsed_viscosity.h"
+#include "grins/spalart_allmaras_viscosity.h"
+#include "grins/grins_physics_names.h"
+#include "grins/turbulence_models_macro.h"
+
+// libMesh
+#include "libmesh/utility.h"
+#include "libmesh/string_to_enum.h"
+#include "libmesh/getpot.h"
+#include "libmesh/fem_system.h"
+
+namespace GRINS
+{
+  template<class Mu>
+  TurbulenceModelsBase<Mu>::TurbulenceModelsBase(const std::string& physics_name, const GetPot& input )
+    : Physics(physics_name, input),
+      _rho(input("Physics/"+incompressible_navier_stokes+"/rho", 1.0)),
+      _mu(input)
+  {
+    return;
+  }
+
+  template<class Mu>
+  TurbulenceModelsBase<Mu>::TurbulenceModelsBase~()
+  {
+    return;
+  }  
+     
+} // namespace GRINS
+
+// Instantiate
+INSTANTIATE_TURBULENCE_MODELS_SUBCLASS(TurbulenceModelsBase);
