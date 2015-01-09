ecafcda37d2a73bbb87c72bad43611426c29f451
Vikram V Garg <vikramvg@mit.edu>
2015-01-08 18:01:22 -0600

    Fixed interior_value to interior_rate for the transient residual functions

--- src/physics/src/inc_navier_stokes_stab_helper.C
+++ src/physics/src/inc_navier_stokes_stab_helper.C
@@ -271,10 +271,10 @@ namespace GRINS
 
   libMesh::RealGradient IncompressibleNavierStokesStabilizationHelper::compute_res_momentum_transient( AssemblyContext& context, unsigned int qp, const libMesh::Real rho ) const
   {
-    libMesh::RealGradient u_dot( context.interior_value(this->_flow_vars.u_var(), qp), context.interior_value(this->_flow_vars.v_var(), qp) );
+    libMesh::RealGradient u_dot( context.interior_rate(this->_flow_vars.u_var(), qp), context.interior_rate(this->_flow_vars.v_var(), qp) );
 
     if(context.get_system().get_mesh().mesh_dimension() == 3)
-      u_dot(2) = context.interior_value(this->_flow_vars.w_var(), qp);
+      u_dot(2) = context.interior_rate(this->_flow_vars.w_var(), qp);
 
     return rho*u_dot;
   }
@@ -288,10 +288,10 @@ namespace GRINS
       libMesh::Real &d_res_Muvw_duvw
     ) const
   {
-    libMesh::RealGradient u_dot( context.interior_value(this->_flow_vars.u_var(), qp), context.interior_value(this->_flow_vars.v_var(), qp) );
+    libMesh::RealGradient u_dot( context.interior_rate(this->_flow_vars.u_var(), qp), context.interior_rate(this->_flow_vars.v_var(), qp) );
 
     if(context.get_system().get_mesh().mesh_dimension() == 3)
-      u_dot(2) = context.interior_value(this->_flow_vars.w_var(), qp);
+      u_dot(2) = context.interior_rate(this->_flow_vars.w_var(), qp);
 
     res_M = rho*u_dot;
     d_res_Muvw_duvw = rho;
