8f3e6f1a2d6a60d90824a252aedc869d6cab4f87
Paul T. Bauman <ptbauman@gmail.com>
2015-03-26 16:09:40 -0400

    Creating PhysicsFactoryHelper
    These functions won't need state, so we'll just pop them into their
    own namespace. Starting with the just wrapping up the one line thats
    in the new_mu_class template function. Will expand to new materials
    section in this function.

---
+++ src/physics/include/grins/physics_factory_helper.h
@@ -0,0 +1,51 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef GRINS_PHYSICS_FACTORY_HELPER_H
+#define GRINS_PHYSICS_FACTORY_HELPER_H
+
+// C++
+#include <string>
+
+// libMesh
+class GetPot;
+
+namespace GRINS
+{
+
+  //! Helper functions for PhysicsFactory
+  /*! There's no state needed for these functions so we put them in
+      a namespace instead of an object. */
+  namespace PhysicsFactoryHelper
+  {
+    //! Determine viscosity model based on given physics name
+    void parse_viscosity_model( const GetPot& input,
+                                const std::string& physics,
+                                std::string& model );
+
+  } // end namespace PhysicsFactoryHelper
+
+} // end namespace GRINS
+
+#endif // GRINS_PHYSICS_FACTORY_HELPER_H
