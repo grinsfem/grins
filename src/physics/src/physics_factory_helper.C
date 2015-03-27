4f062fc6c5dddb0923632a647e58399004025c66
Paul T. Bauman <ptbauman@gmail.com>
2015-03-27 10:35:01 -0400

    Deprecate viscosity_model input variable
    In particular, Physics/IncompressibleNavierStokes/viscosity_model.
    Forthcoming developments will insteady require each Physics to specify
    a material variable and then we will looks in a Materials/material section
    for the models/values.
    
    Make deprecated more obvious

--- src/physics/src/physics_factory_helper.C
+++ src/physics/src/physics_factory_helper.C
@@ -26,6 +26,7 @@
 #include "grins/physics_factory_helper.h"
 
 // GRINS
+#include "grins/common.h"
 #include "grins/grins_physics_names.h"
 
 // libMesh
@@ -37,7 +38,39 @@ namespace GRINS
                                                     const std::string& physics,
                                                     std::string& model )
   {
-    model = input( "Physics/"+incompressible_navier_stokes+"/viscosity_model", "constant" );
+    // Newer, preferred version
+    bool have_material = input.have_variable("Physics/"+physics+"/material");
+
+    // Old deprecated version
+    bool have_viscosity_model = input.have_variable("Physics/"+incompressible_navier_stokes+"/viscosity_model");
+
+    if( have_material && have_viscosity_model )
+      {
+        libmesh_error_msg("Error: Cannot specify both viscosity_model and material.");
+      }
+
+    /* If the user hasn't specified the material or the viscosity_model,
+       we're assuming they're using the old version. */
+    if( !have_viscosity_model && !have_material )
+      {
+        std::string warning = "Warning: Neither viscosity_model norm material were specified.\n";
+        warning += "      We are assuming a constant viscosity model.\n";
+        warning += "      This case is DEPRECATED.\n";
+        warning += "      Please update and specify Physics/"+physics+"/material.\n";
+        grins_warning(warning);
+
+        model = input( "Physics/"+incompressible_navier_stokes+"/viscosity_model", "constant" );
+      }
+
+    if( have_viscosity_model )
+      {
+        std::string warning = "Warning: Option Physics/"+incompressible_navier_stokes+"/viscosity_model is DEPRECATED.\n";
+        warning += "         Please update to use Physics/"+physics+"/material.\n";
+        grins_warning(warning);
+
+        model = input( "Physics/"+incompressible_navier_stokes+"/viscosity_model", "constant" );
+      }
+
     return;
   }
 
