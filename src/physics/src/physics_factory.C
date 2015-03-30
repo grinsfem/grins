5104d7d3f41820776943ce27bd53ea05b43239ff
Nicholas Malaya <nick@ices.utexas.edu>
2015-03-30 17:46:53 -0500

    adding input parsing for new physics class, parsed_fem_source

--- src/physics/src/physics_factory.C
+++ src/physics/src/physics_factory.C
@@ -65,6 +65,7 @@
 #include "grins/grins_physics_names.h"
 #include "grins/constant_source_term.h"
 #include "grins/parsed_source_term.h"
+#include "grins/parsed_fem_source_term.h"
 
 #include "grins/constant_conductivity.h"
 #include "grins/constant_specific_heat.h"
@@ -485,6 +486,11 @@ namespace GRINS
       {
         physics_list[physics_to_add] =
           PhysicsPtr(new ParsedSourceTerm(physics_to_add,input));
+      }
+    else if( physics_to_add == parsed_fem_source_term )
+      {
+        physics_list[physics_to_add] =
+          PhysicsPtr(new ParsedFEMSourceTerm(physics_to_add,input));
       }
     else
       {
