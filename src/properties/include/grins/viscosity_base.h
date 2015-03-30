e9e9659f2baee3ea4cc95f786fd9d2e18315ea79
Paul T. Bauman <ptbauman@gmail.com>
2015-03-30 12:39:23 -0400

    Adding ViscosityBase class
    Mainly to reduce copied code for checking input consistency.

---
+++ src/properties/include/grins/viscosity_base.h
@@ -0,0 +1,51 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+
+#ifndef GRINS_VISCOSITY_BASE_H
+#define GRINS_VISCOSITY_BASE_H
+
+// C++
+#include <string>
+
+// libMesh
+class GetPot;
+
+namespace GRINS
+{
+  //! Base class for viscosity models.
+  /*! Any commonality for all viscosity models goes here. */
+  class ViscosityBase
+  {
+  public:
+    ViscosityBase(){};
+    virtual ~ViscosityBase(){};
+
+  protected:
+    void check_input_consistency( const GetPot& input, const std::string& material ) const;
+  };
+
+} // end namespace GRINS
+
+#endif // GRINS_VISCOSITY_BASE_H
