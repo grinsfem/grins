b0eb2678671f1435b20eadff6849a3ac51a2f8c1
Paul T. Bauman <ptbauman@gmail.com>
2015-04-01 09:35:20 -0400

    First draft of generic solution regression test
    Should've done this way from the beginning; too bad I only learned
    about GetPot command line parsing recently. This way, we just need
    to write shell scripts to call this test with the right command
    line arguments to generate a new regression test.

---
+++ test/generic_solution_regression.C
@@ -0,0 +1,190 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#include "grins_config.h"
+
+// GRINS
+#include "grins/mesh_builder.h"
+#include "grins/simulation.h"
+#include "grins/simulation_builder.h"
+#include "grins/multiphysics_sys.h"
+
+//libMesh
+#include "libmesh/exact_solution.h"
+
+void test_error_norm( libMesh::ExactSolution& exact_sol,
+                      const std::string& system_name,
+                      const std::string& var,
+                      const std::string& norm,
+                      const double tol,
+                      int& return_flag );
+
+int main(int argc, char* argv[])
+{
+  GetPot command_line(argc,argv);
+
+  if( !command_line.search("--input") )
+    {
+      std::cerr << "ERROR: Must specify input file on command line with --input=<file>." << std::endl;
+      exit(1);
+    }
+
+  if( !command_line.search("--soln-data") )
+    {
+      std::cerr << "ERROR: Must specify solution data on command line with --soln-data=<file>." << std::endl;
+      exit(1);
+    }
+
+  if( !command_line.search("--vars") )
+    {
+      std::cerr << "ERROR: Must specify variables on command line with --vars='var1 var2'" << std::endl;
+      exit(1);
+    }
+
+  if( !command_line.search("--norms") )
+    {
+      std::cerr << "ERROR: Must specify variables on command line with --norms='L2 H1'" << std::endl;
+      exit(1);
+    }
+
+  // libMesh input file should be first argument
+  std::string libMesh_input_filename = command_line("--input", "DIE!");
+
+  // Create our GetPot object.
+  GetPot libMesh_inputfile( libMesh_input_filename );
+
+  // Initialize libMesh library.
+  libMesh::LibMeshInit libmesh_init(argc, argv);
+
+  GRINS::SimulationBuilder sim_builder;
+
+  GRINS::Simulation grins( libMesh_inputfile,
+			   sim_builder,
+                           libmesh_init.comm() );
+
+  // Do solve here
+  grins.run();
+
+  // Get equation systems to create ExactSolution object
+  std::tr1::shared_ptr<libMesh::EquationSystems> es = grins.get_equation_system();
+
+   // Create Exact solution object and attach exact solution quantities
+  libMesh::ExactSolution exact_sol(*es);
+
+  libMesh::EquationSystems es_ref( es->get_mesh() );
+
+  // Filename of file where comparison solution is stashed
+  std::string solution_file = command_line("--soln-data", "DIE!");
+  es_ref.read( solution_file );
+
+  exact_sol.attach_reference_solution( &es_ref );
+
+  // Now grab the variables for which we want to compare
+  unsigned int n_vars = command_line.vector_variable_size("--vars");
+  std::vector<std::string> vars(n_vars);
+  for( unsigned int v = 0; v < n_vars; v++ )
+    {
+      vars[v] = command_line("--vars", "DIE!", v);
+    }
+
+  // Now grab the norms to compute for each variable error
+  unsigned int n_norms = command_line.vector_variable_size("--norms");
+  std::vector<std::string> norms(n_vars);
+  for( unsigned int n = 0; n < n_norms; n++ )
+    {
+      norms[n] = command_line("--norms", "DIE!", n);
+      if( norms[n] != std::string("L2") &&
+          norms[n] != std::string("H1") )
+        {
+          std::cerr << "ERROR: Invalid norm input " << norms[n] << std::endl
+                    << "       Valid values are: L2" << std::endl
+                    << "                         H1" << std::endl;
+        }
+    }
+
+  const std::string& system_name = grins.get_multiphysics_system_name();
+
+  // Now compute error for each variable
+  for( unsigned int v = 0; v < n_vars; v++ )
+    {
+      exact_sol.compute_error(system_name, vars[v]);
+    }
+
+  int return_flag = 0;
+
+  double tol = 1.0e-10;
+
+  // Now test error for each variable, for each norm
+  for( unsigned int v = 0; v < n_vars; v++ )
+    {
+      for( unsigned int n = 0; n < n_norms; n++ )
+        {
+          test_error_norm( exact_sol, system_name, vars[v], norms[n], tol, return_flag );
+        }
+    }
+
+  return return_flag;
+}
+
+void test_error_norm( libMesh::ExactSolution& exact_sol,
+                      const std::string& system_name,
+                      const std::string& var,
+                      const std::string& norm,
+                      const double tol,
+                      int& return_flag )
+{
+  // We don't set return_flag unless we are setting it 1
+  // since this function gets called multiple times and we don't
+  // want to overwrite a previous "fail" (return_flag = 1) with
+  // a "pass" (return_flag = 0)
+
+  double error = 0.0;
+
+  if( norm == std::string("L2") )
+    {
+      error = exact_sol.l2_error(system_name, var);
+    }
+  else if( norm == std::string("H1") )
+    {
+      error = exact_sol.h1_error(system_name, var);
+    }
+  else
+    {
+      std::cerr << "ERROR: Invalid norm " << norm << std::endl;
+      exit(1);
+    }
+
+  if( error > tol )
+    {
+      return_flag = 1;
+
+      std::cerr << "Tolerance exceeded for generic regression test!" << std::endl
+                << "tolerance     = " << tol << std::endl
+                << "norm of error = " << error << std::endl
+                << "norm type     = " << norm << std::endl
+                << "var           = " << var << std::endl;
+    }
+
+  return;
+}
