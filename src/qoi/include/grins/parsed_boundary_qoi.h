2df9fa5f70db60e05a8d37b89a0d168e978eb346
Roy H. Stogner <roystgnr@ices.utexas.edu>
2015-05-22 13:15:32 -0500

    Add ParsedBoundaryQoI code

---
+++ src/qoi/include/grins/parsed_boundary_qoi.h
@@ -0,0 +1,100 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+
+#ifndef GRINS_PARSED_BOUNDARY_QOI_H
+#define GRINS_PARSED_BOUNDARY_QOI_H
+
+// GRINS
+#include "grins/qoi_base.h"
+#include "grins/variable_name_defaults.h"
+
+// libMesh
+#include "libmesh/fem_function_base.h"
+
+namespace GRINS
+{
+  //! Parsed Boundary QoI
+  /*!
+    This class implements a QoI that is an arbitrary integral of a
+    parsed function on the boundary of the domain.
+   */
+  class ParsedBoundaryQoI : public QoIBase
+  {
+  public:
+
+    //! Constructor
+    /*! Constructor takes GetPot object to read any input options associated
+        with this QoI */
+    ParsedBoundaryQoI( const std::string& qoi_name );
+
+    virtual ~ParsedBoundaryQoI();
+
+    //! Required to provide clone (deep-copy) for adding QoI object to libMesh objects.
+    virtual QoIBase* clone() const;
+
+    virtual bool assemble_on_interior() const;
+
+    virtual bool assemble_on_sides() const;
+
+    //! Initialize local variables
+    virtual void init( const GetPot& input, const MultiphysicsSystem& system );
+
+    virtual void init_context( AssemblyContext& context );
+
+    //! Compute the qoi value.
+    virtual void side_qoi( AssemblyContext& context,
+                           const unsigned int qoi_index );
+
+    //! Compute the qoi derivative with respect to the solution.
+    virtual void side_qoi_derivative( AssemblyContext& context,
+                                      const unsigned int qoi_index );
+
+  protected:
+
+    libMesh::AutoPtr<libMesh::FEMFunctionBase<libMesh::Number> >
+      qoi_functional;
+
+    //! Manual copy constructor due to the AutoPtr
+    ParsedBoundaryQoI(const ParsedBoundaryQoI& original);
+
+  private:
+    //! User never call default constructor.
+    ParsedBoundaryQoI();
+
+  };
+
+  inline
+  bool ParsedBoundaryQoI::assemble_on_interior() const
+  {
+    return false;
+  }
+
+  inline
+  bool ParsedBoundaryQoI::assemble_on_sides() const
+  {
+    return true;
+  }
+}
+#endif //GRINS_PARSED_BOUNDARY_QOI_H
---
+++ src/qoi/src/parsed_boundary_qoi.C
@@ -0,0 +1,160 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+
+// This class
+#include "grins/parsed_boundary_qoi.h"
+
+// GRINS
+#include "grins/multiphysics_sys.h"
+#include "grins/assembly_context.h"
+
+// libMesh
+#include "libmesh/getpot.h"
+#include "libmesh/fem_system.h"
+#include "libmesh/quadrature.h"
+#include "libmesh/fe_base.h"
+#include "libmesh/parsed_fem_function.h"
+
+namespace GRINS
+{
+  ParsedBoundaryQoI::ParsedBoundaryQoI( const std::string& qoi_name )
+    : QoIBase(qoi_name) {}
+
+  ParsedBoundaryQoI::ParsedBoundaryQoI( const ParsedBoundaryQoI& original )
+    : QoIBase(original.name())
+  {
+    if (original.qoi_functional.get())
+      this->qoi_functional = original.qoi_functional->clone();
+  }
+
+  ParsedBoundaryQoI::~ParsedBoundaryQoI() {}
+
+  QoIBase* ParsedBoundaryQoI::clone() const
+  {
+    return new ParsedBoundaryQoI( *this );
+  }
+
+  void ParsedBoundaryQoI::init( const GetPot& input, const MultiphysicsSystem& system )
+  {
+    std::string qoi_functional_string =
+      input("QoI/ParsedBoundary/qoi_functional", std::string("0"));
+
+    if (qoi_functional_string == "0")
+      libmesh_error_msg("Error! Zero ParsedBoundaryQoI specified!" <<
+                        std::endl);
+
+    this->qoi_functional.reset
+      (new libMesh::ParsedFEMFunction<libMesh::Number>
+       (system, qoi_functional_string));
+  }
+
+  void ParsedBoundaryQoI::init_context( AssemblyContext& context )
+  {
+    libMesh::FEBase* side_fe;
+    context.get_side_fe<libMesh::Real>(0, side_fe);
+    side_fe->get_JxW();
+    side_fe->get_xyz();
+
+    qoi_functional->init_context(context);
+  }
+
+  void ParsedBoundaryQoI::side_qoi( AssemblyContext& context,
+                                    const unsigned int qoi_index )
+  {
+    libMesh::FEBase* side_fe;
+    context.get_side_fe<libMesh::Real>(0, side_fe);
+    const std::vector<libMesh::Real> &JxW = side_fe->get_JxW();
+
+    const std::vector<libMesh::Point>& x_qp = side_fe->get_xyz();
+
+    unsigned int n_qpoints = context.get_side_qrule().n_points();
+
+    /*! \todo Need to generalize this to the multiple QoI case */
+    libMesh::Number& qoi = context.get_qois()[qoi_index];
+
+    for( unsigned int qp = 0; qp != n_qpoints; qp++ )
+      {
+        const libMesh::Number func_val =
+          (*qoi_functional)(context, x_qp[qp], context.get_time());
+
+        qoi += func_val * JxW[qp];
+      }
+  }
+
+  void ParsedBoundaryQoI::side_qoi_derivative( AssemblyContext& context,
+                                               const unsigned int qoi_index )
+  {
+    libMesh::FEBase* side_fe;
+    context.get_side_fe<libMesh::Real>(0, side_fe);
+    const std::vector<libMesh::Real> &JxW = side_fe->get_JxW();
+
+    const std::vector<libMesh::Point>& x_qp = side_fe->get_xyz();
+
+    // Local DOF count and quadrature point count
+    const unsigned int n_u_dofs = context.get_dof_indices().size();
+
+    unsigned int n_qpoints = context.get_side_qrule().n_points();
+
+    // Local solution vector - non-const version for finite
+    // differenting purposes
+    libMesh::DenseVector<libMesh::Number>& elem_solution =
+      const_cast<libMesh::DenseVector<libMesh::Number>&>
+        (context.get_elem_solution());
+
+    /*! \todo Need to generalize this to the multiple QoI case */
+    libMesh::DenseVector<libMesh::Number> &Qu =
+      context.get_qoi_derivatives()[qoi_index];
+
+    for( unsigned int qp = 0; qp != n_qpoints; qp++ )
+      {
+        // Central finite differencing to approximate derivatives.
+        // FIXME - we should hook the FParserAD stuff into
+        // ParsedFEMFunction
+
+        for( unsigned int i = 0; i != n_u_dofs; ++i )
+          {
+            libMesh::Number &current_solution = elem_solution(i);
+            const libMesh::Number original_solution = current_solution;
+
+            current_solution = original_solution + libMesh::TOLERANCE;
+
+            const libMesh::Number plus_val =
+              (*qoi_functional)(context, x_qp[qp], context.get_time());
+
+            current_solution = original_solution - libMesh::TOLERANCE;
+
+            const libMesh::Number minus_val =
+              (*qoi_functional)(context, x_qp[qp], context.get_time());
+
+            Qu(i) += (plus_val - minus_val) *
+                     (0.5 / libMesh::TOLERANCE) * JxW[qp];
+
+            // Don't forget to restore the correct solution...
+            current_solution = original_solution;
+          }
+      }
+  }
+
+} //namespace GRINS
