5dd77cc66c21d730065dbb57bfd7e1e9da6d34fa
Paul T. Bauman <ptbauman@gmail.com>
2015-09-18 18:07:48 -0400

    Add ParsedPropertyBase class
    This will collect common functionality used in already existing
    classes, i.e. ParsedViscosity and ParsedConductivity, and will
    facilitate other subclasses.

---
+++ src/properties/src/parsed_property_base.C
@@ -0,0 +1,41 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+// This class
+#include "grins/parsed_property_base.h"
+
+namespace GRINS
+{
+  bool ParsedPropertyBase::check_func_nonzero( const std::string& function ) const
+  {
+    bool is_nonzero = true;
+
+    if (function == std::string("0"))
+      {
+        is_nonzero = false;
+      }
+
+    return is_nonzero;
+  }
+} // end namespace GRINS
