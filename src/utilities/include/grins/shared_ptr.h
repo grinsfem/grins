c9a34fa1e41afd748c20bc9313d63d82ae75592b
Paul T. Bauman <ptbauman@gmail.com>
2015-11-26 00:14:53 -0500

    Add GRINS::SharedPtr
    We just subclass shared_ptr if we have a C++11 compliant compiler.
    We actually just piggy back on the result of libMesh's results. If
    we don't have std::shared_ptr, we fall back to Boost. If we don't have
    that, we error.

---
+++ src/utilities/include/grins/shared_ptr.h
@@ -0,0 +1,53 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef GRINS_SHARED_PTR_H
+#define GRINS_SHARED_PTR_H
+
+#include "grins_config.h"
+#include "libmesh/libmesh_config.h"
+
+// Here, we just piggy back on libMesh's test for shared_ptr
+#ifdef LIBMESH_HAVE_CXX11_SHARED_PTR
+#include <memory>
+#elif GRINS_HAVE_BOOST_SHARED_PTR_HPP
+#include <boost/shared_ptr.hpp>
+#endif
+
+namespace GRINS
+{
+#ifdef LIBMESH_HAVE_CXX11_SHARED_PTR
+  template<typename T>
+  class SharedPtr : public std::shared_ptr<T>
+  {};
+#elif GRINS_HAVE_BOOST_SHARED_PTR_HPP
+  template<typename T>
+  class SharedPtr : public boost::shared_ptr<T>
+  {};
+#else
+#     error "No valid definition for shared_ptr found!"
+#endif
+}
+
+#endif // GRINS_SHARED_PTR_H
