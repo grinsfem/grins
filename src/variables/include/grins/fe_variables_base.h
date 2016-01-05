74c84b32ffd6fd8300ce04988de60eed6dcea9a7
Paul T. Bauman <ptbauman@gmail.com>
2016-01-05 13:17:11 -0500

    Add FEVariablesBase
    To be used in the same vein as VariablesBase

---
+++ src/variables/include/grins/fe_variables_base.h
@@ -0,0 +1,59 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+
+#ifndef GRINS_FE_VARIABLES_BASE_H
+#define GRINS_FE_VARIABLES_BASE_H
+
+// C++
+#include <vector>
+#include <string>
+
+// GRINS
+#include "grins/grins_enums.h"
+
+// libMesh
+#include "libmesh/enum_order.h"
+#include "libmesh/enum_fe_family.h"
+
+namespace GRINS
+{
+  class FEVariablesBase
+  {
+  public:
+
+    FEVariablesBase(){};
+
+    ~FEVariablesBase(){};
+
+  protected:
+
+    std::vector<GRINSEnums::FEFamily> _family;
+
+    std::vector<GRINSEnums::Order> _order;
+
+  };
+} // end namespace GRINS
+
+#endif // GRINS_FE_VARIABLES_BASE_H
