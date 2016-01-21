0d6ee5d70639486943b605313b44b8e7b8377b41
Paul T. Bauman <ptbauman@gmail.com>
2016-01-21 16:27:35 -0500

    Encapsulate solver names
    Mainly for consist use everywhere we need them. I think I like this
    better than just a header with strings in a namespace because this
    will be more easily found in Doxygen documentation.

---
+++ src/solver/include/grins/solver_names.h
@@ -0,0 +1,50 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+// C++
+#include <string>
+
+namespace GRINS
+{
+  class SolverNames
+  {
+  public:
+
+    static const std::string steady_solver()
+    { return "grins_steady_solver"; }
+
+    static const std::string unsteady_solver()
+    { return "grins_unsteady_solver"; }
+
+    static const std::string steady_mesh_adaptive_solver()
+    { return "grins_steady_mesh_adaptive_solver"; }
+
+    static const std::string unsteady_mesh_adaptive_solver()
+    { return "grins_unsteady_mesh_adaptive_solver"; }
+
+    static const std::string displacement_continuation()
+    { return "displacement_continuation"; }
+
+  };
+} // end namespace GRINS
