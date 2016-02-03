5732b83643390eb1e65e4e23b6376ef62ed1c851
Paul T. Bauman <ptbauman@gmail.com>
2016-02-03 11:24:35 -0500

    Add SimulationParsing
    We'll use this to start encapsulating things the Simulation
    object parses. For now, just populating it with the restart bits
    because that's what I need immediately.

---
+++ src/solver/include/grins/simulation_parsing.h
@@ -0,0 +1,58 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef GRINS_SIMULATION_PARSING_H
+#define GRINS_SIMULATION_PARSING_H
+
+// C++
+#include <string>
+
+// libMesh
+#include "libmesh/getpot.h"
+
+namespace GRINS
+{
+  class SimulationParsing
+  {
+  public:
+
+    SimulationParsing(){};
+
+    ~SimulationParsing(){};
+
+    static bool have_restart( const GetPot& input )
+    { return input.have_variable( SimulationParsing::restart_input_option() ); }
+
+    static std::string restart_file( const GetPot& input )
+    { return input( SimulationParsing::restart_input_option(), "none" ); }
+
+  private:
+
+    static std::string restart_input_option()
+    { return "restart-options/restart_file"; }
+  };
+
+} // end namespace GRINS
+
+#endif // GRINS_SIMULATION_PARSING_H
