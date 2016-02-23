19308bb144f27f5f69a31203a306817a271fae09
Paul T. Bauman <ptbauman@gmail.com>
2016-02-22 21:33:32 -0500

    Use grins app directly in xfail tests
    Remove useless ufo_unit test

--- test/error_xfail/ins_invalid_pin_location.sh
+++ test/error_xfail/ins_invalid_pin_location.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-PROG="${GRINS_TEST_DIR}/generic_solution_regression"
+PROG="${GRINS_BUILDSRC_DIR}/grins"
 
 INPUT="${GRINS_TEST_INPUT_DIR}/ins_invalid_pin_location_unit.in"
 
-# THIS IS DUMMY, the test is expected to fail before getting to the data comparison
-DATA="${GRINS_TEST_DATA_DIR}/laplace_parsed_source_regression.xdr"
-
-${LIBMESH_RUN:-} $PROG input=$INPUT soln-data=$DATA vars='T' norms='L2 H1' tol='1.0e-10'
+${LIBMESH_RUN:-} $PROG $INPUT
--- test/warn_only_ufo_unit.sh
+++ test/warn_only_ufo_unit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROG="${GRINS_TEST_DIR}/ufo_unit"
+PROG="${GRINS_BUILDSRC_DIR}/grins"
 
 INPUT="${GRINS_TEST_INPUT_DIR}/ufo_unit.in"
 
