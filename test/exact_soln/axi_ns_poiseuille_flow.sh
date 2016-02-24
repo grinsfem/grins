ee5d58c589d1f112e430ea525f50d7385878744d
Paul T. Bauman <ptbauman@gmail.com>
2016-02-23 22:05:47 -0500

    Convert axi_ns_poiseuille_flow exact_soln test
    Convert axi_ns_poiseuille_flow test to use the generic exact solution
    app.

--- test/exact_soln/axi_ns_poiseuille_flow.sh
+++ test/exact_soln/axi_ns_poiseuille_flow.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-PROG="${GRINS_TEST_DIR}/axi_ns_poiseuille_flow"
+set -e
 
 INPUT="${GRINS_TEST_INPUT_DIR}/axi_poiseuille_flow_input.in"
+TESTDATA="./axi_ns_poiseuille_flow.xdr"
 
-PETSC_OPTIONS="-pc_factor_levels 4 -sub_pc_factor_levels 4"
+# First run the case with grins
+${LIBMESH_RUN:-} ${GRINS_BUILDSRC_DIR}/grins $INPUT
 
-${LIBMESH_RUN:-} $PROG $INPUT $PETSC_OPTIONS 
+# Now run the test part to make sure we're getting the correct thing
+${GRINS_TEST_DIR}/generic_exact_solution_testing_app input=$INPUT vars='u_r u_z p' norms='L2' tol='1.0e-10' u_r_L2_error='1.0e-10' u_z_L2_error='1.0e-10' p_L2_error='5.13691e-09' u_r_exact_soln='0.0' u_z_exact_soln='100.0/4.0*(1-x^2)' p_exact_soln='100*(1-y)' test_data=$TESTDATA
+
+# Now remove the test turd
+rm $TESTDATA
