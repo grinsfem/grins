19a22e2487c4adb27d0daaef4170d85dc55a7b93
Paul T. Bauman <ptbauman@gmail.com>
2016-03-07 15:21:46 -0500

    Add CanteraNASA9ThermoTest

---
+++ test/interface/cantera_thermo.C
@@ -0,0 +1,91 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#include "grins_config.h"
+
+#ifdef GRINS_HAVE_CPPUNIT
+#ifdef GRINS_HAVE_CANTERA
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestCase.h>
+
+#include "grins_test_paths.h"
+#include "air_nasa_poly_base.h"
+#include "cantera_test_base.h"
+#include "thermochem_test_common.h"
+#include "testing_utils.h"
+
+// GRINS
+#include "grins/cantera_evaluator.h"
+
+// C++
+#include <sstream>
+
+namespace GRINSTesting
+{
+  class CanteraNASA9ThermoTest : public CppUnit::TestCase,
+                                 public CanteraTestBase,
+                                 public AirNASA9TestBase
+  {
+  public:
+    CPPUNIT_TEST_SUITE( CanteraNASA9ThermoTest );
+
+    CPPUNIT_TEST( test_cp );
+    CPPUNIT_TEST( test_hs );
+
+    CPPUNIT_TEST_SUITE_END();
+
+  public:
+
+    void setUp()
+    {
+      std::string input_file = std::string(GRINS_TEST_SRCDIR)+"/input_files/cantera_chem_thermo.in";
+      this->init_cantera(input_file, "TestMaterial");
+
+      _N2_idx = _cantera_mixture->species_index("N2");
+      _O2_idx = _cantera_mixture->species_index("O2");
+      _N_idx = _cantera_mixture->species_index("N");
+      _O_idx = _cantera_mixture->species_index("O");
+      _NO_idx = _cantera_mixture->species_index("NO");
+    }
+
+    void test_cp()
+    {
+      this->test_cp_common<GRINS::CanteraMixture,GRINS::CanteraEvaluator>
+        ( *_cantera_mixture, (*this), 1.0e-4 /*relative tolerance*/ );
+    }
+
+    void test_hs()
+    {
+      this->test_h_common<GRINS::CanteraMixture,GRINS::CanteraEvaluator>
+        ( *_cantera_mixture, (*this), 1.0e-4 /*relative tolerance*/ );
+    }
+  };
+
+  CPPUNIT_TEST_SUITE_REGISTRATION( CanteraNASA9ThermoTest );
+
+} // end namespace GRINSTesting
+
+#endif // GRINS_HAVE_CANTERA
+#endif // GRINS_HAVE_CPPUNIT
