000262c5fff36d656b2e965c2531cda26052a73a
Paul T. Bauman <ptbauman@gmail.com>
2016-03-09 19:07:55 -0500

    Add PhysicsFactoryPlaneStressSolids
    This is the physics factory class for handling solid mechanics Physics
    that depend on a plane stress stress-strain law.

---
+++ src/physics/include/grins/physics_factory_plane_stress_solids.h
@@ -0,0 +1,53 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef GRINS_PHYSICS_FACTORY_PLANE_STRESS_SOLIDS_H
+#define GRINS_PHYSICS_FACTORY_PLANE_STRESS_SOLIDS_H
+
+// GRINS
+#include "grins/physics_factory_with_core.h"
+
+namespace GRINS
+{
+  template<template<typename> class DerivedPhysics>
+  class PhysicsFactoryPlaneStressSolids : public PhysicsFactoryWithCore
+  {
+  public:
+    PhysicsFactoryPlaneStressSolids( const std::string& physics_name,
+                                     const std::string& core_physics_name )
+      : PhysicsFactoryWithCore(physics_name,core_physics_name)
+    {}
+
+    ~PhysicsFactoryPlaneStressSolids(){};
+
+  protected:
+
+    virtual libMesh::UniquePtr<Physics> build_physics( const GetPot& input,
+                                                       const std::string& physics_name );
+
+  };
+
+} // end namespace GRINS
+
+#endif // GRINS_PHYSICS_FACTORY_PLANE_STRESS_SOLIDS_H
