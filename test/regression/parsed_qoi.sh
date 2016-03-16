94e8b1797d9458db6d3c017b3cb16361e4279e16
Roy H. Stogner <roystgnr@ices.utexas.edu>
2016-03-16 10:45:31 -0500

    More robust convergence for regression test
    With the newest PETSc this doesn't converge on 6 processors unless we
    crank up the preconditioner a bit.

--- test/regression/parsed_qoi.sh
+++ test/regression/parsed_qoi.sh
@@ -4,5 +4,7 @@ PROG="${GRINS_BUILDSRC_DIR}/grins"
 
 INPUT="${GRINS_TEST_INPUT_DIR}/parsed_qoi.in"
 
-${LIBMESH_RUN:-} $PROG $INPUT
+PETSC_OPTIONS="-pc_factor_levels 4 -sub_pc_factor_levels 4"
+
+${LIBMESH_RUN:-} $PROG $INPUT $PETSC_OPTIONS
 
