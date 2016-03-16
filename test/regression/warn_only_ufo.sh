ba6b5dee82801505deab49ca3beff7dcb5abe110
Roy H. Stogner <roystgnr@ices.utexas.edu>
2016-03-16 11:11:20 -0500

    Another solver robustness improvement

--- test/regression/warn_only_ufo.sh
+++ test/regression/warn_only_ufo.sh
@@ -4,6 +4,8 @@ PROG="${GRINS_BUILDSRC_DIR}/grins"
 
 INPUT="${GRINS_TEST_INPUT_DIR}/ufo_unit.in"
 
+PETSC_OPTIONS="-pc_factor_levels 4 -sub_pc_factor_levels 4"
+
 GRINS_OPTIONS="--warn-only-unused-var"
 
-${LIBMESH_RUN:-} $PROG $INPUT $GRINS_OPTIONS
+${LIBMESH_RUN:-} $PROG $INPUT $GRINS_OPTIONS $PETSC_OPTIONS
