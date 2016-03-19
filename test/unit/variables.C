9fb707be12ea4c691b6bf8dcc02dd2d9494e1c6a
Paul T. Bauman <ptbauman@gmail.com>
2016-03-19 14:01:02 -0400

    Add velocity variables 3D testing

--- test/unit/variables.C
+++ test/unit/variables.C
@@ -49,7 +49,7 @@ namespace GRINSTesting
     CPPUNIT_TEST_SUITE( VariablesTest );
 
     CPPUNIT_TEST( test_velocity_2d );
-    //CPPUNIT_TEST( test_velocity_3d );
+    CPPUNIT_TEST( test_velocity_3d );
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -90,6 +90,36 @@ namespace GRINSTesting
       }
     }
 
+    void test_velocity_3d()
+    {
+      std::string filename = std::string(GRINS_TEST_UNIT_INPUT_SRCDIR)+"/variables_3d.in";
+      this->setup_multiphysics_system(filename);
+
+      // This will add the variables to the system
+      {
+        GRINS::VelocityFEVariables vel_vars(*_input,GRINS::PhysicsNaming::incompressible_navier_stokes());
+        vel_vars.init(_system);
+        CPPUNIT_ASSERT_EQUAL((unsigned int)3,_system->n_vars());
+
+        const std::vector<std::string>& var_names = vel_vars.active_var_names();
+        this->test_vel_var_names_3d(var_names);
+
+        // Verify the FE part
+        this->test_vel_fe_3d(*_system);
+      }
+
+      // Now we should be able to also use a basic VelocityVariables class
+      // and get the right var names out once the variables are added to the
+      // system
+      {
+        GRINS::VelocityVariables vel_vars(*_input);
+        vel_vars.init_vars(_system);
+
+        const std::vector<std::string>& var_names = vel_vars.active_var_names();
+        this->test_vel_var_names_3d(var_names);
+      }
+    }
+
   private:
 
     void test_vel_var_names_2d( const std::vector<std::string>& var_names )
@@ -100,6 +130,15 @@ namespace GRINSTesting
       CPPUNIT_ASSERT_EQUAL(std::string("Uy"),var_names[1]);
     }
 
+    void test_vel_var_names_3d( const std::vector<std::string>& var_names )
+    {
+      // For 3-D, we should have 3 components
+      CPPUNIT_ASSERT_EQUAL(3,(int)var_names.size());
+      CPPUNIT_ASSERT_EQUAL(std::string("Ux"),var_names[0]);
+      CPPUNIT_ASSERT_EQUAL(std::string("Uy"),var_names[1]);
+      CPPUNIT_ASSERT_EQUAL(std::string("Uz"),var_names[2]);
+    }
+
     void test_vel_fe_2d( const libMesh::System& system )
     {
       libMesh::Order order = system.variable_type("Ux").order;
@@ -108,6 +147,14 @@ namespace GRINSTesting
 
       order = system.variable_type("Uy").order;
       CPPUNIT_ASSERT_EQUAL(GRINSEnums::LAGRANGE,system.variable_type("Uy").family);
+      CPPUNIT_ASSERT_EQUAL(GRINSEnums::FIRST,order);
+    }
+
+    void test_vel_fe_3d( const libMesh::System& system )
+    {
+      this->test_vel_fe_2d(system);
+      libMesh::Order order = system.variable_type("Uz").order;
+      CPPUNIT_ASSERT_EQUAL(GRINSEnums::LAGRANGE,system.variable_type("Uz").family);
       CPPUNIT_ASSERT_EQUAL(GRINSEnums::FIRST,order);
     }
 
