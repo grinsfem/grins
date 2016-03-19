a73b5f3ae772452e712c17bc74dfe2c1296e5da0
Paul T. Bauman <ptbauman@gmail.com>
2016-03-19 00:31:26 -0400

    Move split_string test to CppUnit-based one

--- test/unit/split_string.C
+++ test/unit/split_string.C
@@ -22,109 +22,106 @@
 //
 //-----------------------------------------------------------------------el-
 
+#include "grins_config.h"
+
+#ifdef GRINS_HAVE_CPPUNIT
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestCase.h>
+
 #include <string>
 #include <vector>
 #include <iostream>
 
 #include "grins/string_utils.h"
 
-bool test_string( const std::vector<std::string>& test,
-                  const std::vector<std::string>& exact );
-
-int main()
+namespace GRINSTesting
 {
-  int return_flag = 0;
+  class StringUtilitiesTest : public CppUnit::TestCase
+  {
+  public:
+    CPPUNIT_TEST_SUITE( StringUtilitiesTest );
 
-  std::string str_1("N->N2");
-  std::vector<std::string> test_1_split_exact(2);
-  test_1_split_exact[0] = std::string("N");
-  test_1_split_exact[1] = std::string("N2");
+    CPPUNIT_TEST( test_split_string );
 
-  std::vector<std::string> str_1_split;
-  GRINS::StringUtilities::split_string( str_1, "->", str_1_split);
-  if( !test_string( str_1_split, test_1_split_exact ) )
-      return_flag = 1;
+    CPPUNIT_TEST_SUITE_END();
 
-  std::string str_2("N+C(s)->CN");
-  std::vector<std::string> test_2_split_exact(2);
-  test_2_split_exact[0] = std::string("N+C(s)");
-  test_2_split_exact[1] = std::string("CN");
+  public:
 
-  std::vector<std::string> str_2_split;
-  GRINS::StringUtilities::split_string( str_2, "->", str_2_split);
-  if( !test_string( str_2_split, test_2_split_exact ) )
-      return_flag = 1;
-
-  std::string str_3("u:v:w:T:p:w_N:w_N2:p0");
-  std::vector<std::string> test_3_split_exact(8);
-  test_3_split_exact[0] = std::string("u");
-  test_3_split_exact[1] = std::string("v");
-  test_3_split_exact[2] = std::string("w");
-  test_3_split_exact[3] = std::string("T");
-  test_3_split_exact[4] = std::string("p");
-  test_3_split_exact[5] = std::string("w_N");
-  test_3_split_exact[6] = std::string("w_N2");
-  test_3_split_exact[7] = std::string("p0");
-
-  std::vector<std::string> str_3_split;
-  GRINS::StringUtilities::split_string( str_3, ":", str_3_split);
-  if( !test_string( str_3_split, test_3_split_exact ) )
-      return_flag = 1;
-
-  std::string str_4("u v w T p w_N w_N2 p0");
-  std::vector<std::string> test_4_split_exact(8);
-  test_4_split_exact[0] = std::string("u");
-  test_4_split_exact[1] = std::string("v");
-  test_4_split_exact[2] = std::string("w");
-  test_4_split_exact[3] = std::string("T");
-  test_4_split_exact[4] = std::string("p");
-  test_4_split_exact[5] = std::string("w_N");
-  test_4_split_exact[6] = std::string("w_N2");
-  test_4_split_exact[7] = std::string("p0");
-
-  std::vector<std::string> str_4_split;
-  GRINS::StringUtilities::split_string( str_4, " ", str_4_split);
-  if( !test_string( str_4_split, test_4_split_exact ) )
-      return_flag = 1;
-
-  return return_flag;
-}
-
-bool test_string( const std::vector<std::string>& test,
-                  const std::vector<std::string>& exact )
-{
-  bool test_passed = true;
-
-  if( test.size() != exact.size() )
+    void test_split_string()
     {
-      test_passed = false;
-      std::cerr << "Error: split_string size mismatch!" << std::endl
-                << "Test string size = " << test.size() << std::endl
-                << "Exact string size = " << exact.size() << std::endl;
+      {
+        std::string str_1("N->N2");
+        std::vector<std::string> test_1_split_exact(2);
+        test_1_split_exact[0] = std::string("N");
+        test_1_split_exact[1] = std::string("N2");
+
+        std::vector<std::string> str_1_split;
+        GRINS::StringUtilities::split_string( str_1, "->", str_1_split);
+        this->test_string( str_1_split, test_1_split_exact );
+      }
+
+      {
+        std::string str_2("N+C(s)->CN");
+        std::vector<std::string> test_2_split_exact(2);
+        test_2_split_exact[0] = std::string("N+C(s)");
+        test_2_split_exact[1] = std::string("CN");
+
+        std::vector<std::string> str_2_split;
+        GRINS::StringUtilities::split_string( str_2, "->", str_2_split);
+        this->test_string( str_2_split, test_2_split_exact );
+      }
+
+      {
+        std::string str_3("u:v:w:T:p:w_N:w_N2:p0");
+        std::vector<std::string> test_3_split_exact(8);
+        test_3_split_exact[0] = std::string("u");
+        test_3_split_exact[1] = std::string("v");
+        test_3_split_exact[2] = std::string("w");
+        test_3_split_exact[3] = std::string("T");
+        test_3_split_exact[4] = std::string("p");
+        test_3_split_exact[5] = std::string("w_N");
+        test_3_split_exact[6] = std::string("w_N2");
+        test_3_split_exact[7] = std::string("p0");
+
+        std::vector<std::string> str_3_split;
+        GRINS::StringUtilities::split_string( str_3, ":", str_3_split);
+        this->test_string( str_3_split, test_3_split_exact );
+      }
+
+      {
+        std::string str_4("u v w T p w_N w_N2 p0");
+        std::vector<std::string> test_4_split_exact(8);
+        test_4_split_exact[0] = std::string("u");
+        test_4_split_exact[1] = std::string("v");
+        test_4_split_exact[2] = std::string("w");
+        test_4_split_exact[3] = std::string("T");
+        test_4_split_exact[4] = std::string("p");
+        test_4_split_exact[5] = std::string("w_N");
+        test_4_split_exact[6] = std::string("w_N2");
+        test_4_split_exact[7] = std::string("p0");
+
+        std::vector<std::string> str_4_split;
+        GRINS::StringUtilities::split_string( str_4, " ", str_4_split);
+        this->test_string( str_4_split, test_4_split_exact );
+      }
     }
 
-  if( test_passed )
+  private:
+
+    void test_string( const std::vector<std::string>& test,
+                      const std::vector<std::string>& exact )
     {
+      CPPUNIT_ASSERT_EQUAL(test.size(), exact.size() );
+
       for( unsigned int s = 0; s < test.size(); s++ )
-        {
-          if( test[s] != exact[s] )
-            {
-              test_passed = false;
-              break;
-            }
-        }
-
-      if( !test_passed)
-        {
-          std::cerr << "Error: split_string value mismatch!" << std::endl
-                    << "Test string, Exact string" << std::endl;
-
-          for( unsigned int s = 0; s < test.size(); s++ )
-            {
-              std::cerr << test[s] << ", " << exact[s] << std::endl;
-            }
-        }
+        CPPUNIT_ASSERT_EQUAL( test[s], exact[s] );
     }
 
-  return test_passed;
-}
+  };
+
+  CPPUNIT_TEST_SUITE_REGISTRATION( StringUtilitiesTest );
+
+} // end namespace GRINSTesting
+
+#endif // GRINS_HAVE_CPPUNIT
