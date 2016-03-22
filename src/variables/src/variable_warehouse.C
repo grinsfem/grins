09c091fb4a8fb9d76e8a6113d8438cabcf1f1d53
Paul T. Bauman <ptbauman@gmail.com>
2016-03-22 10:10:29 -0400

    Add VariableWarehouse
    Place to centrally register FEVariableBase classes. I expect that
    we'll need to change the API once we support per-subdomain variables
    so I put this in a GRINSPrivate namespace. The intended use case of
    this will be in the boundary condition refactoring downstream.

---
+++ src/variables/src/variable_warehouse.C
@@ -0,0 +1,38 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+// This class
+#include "grins/variable_warehouse.h"
+
+namespace GRINS
+{
+  namespace GRINSPrivate
+  {
+    std::map<std::string,const FEVariablesBase*>& VariableWarehouse::var_map()
+    {
+      static std::map<std::string, const FEVariablesBase*> _var_map;
+      return _var_map;
+    }
+  } // end namespace GRINSPrivate
+} // end namespace GRINS
