f01102bbafeb252d3d199f5a07d1bb3f4ab9dc91
Paul T. Bauman <ptbauman@gmail.com>
2016-03-22 18:25:59 -0400

    Add ErrorEstimatorOptions

---
+++ src/strategies/include/grins/error_estimator_options.h
@@ -0,0 +1,79 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef GRINS_ERROR_ESTIMATOR_OPTIONS_H
+#define GRINS_ERROR_ESTIMATOR_OPTIONS_H
+
+// C++
+#include <string>
+
+// libmMesh forward declarations
+class GetPot;
+
+namespace GRINS
+{
+  //! Container for ErrorEstimator options
+  class ErrorEstimatorOptions
+  {
+  public:
+
+    ErrorEstimatorOptions( const GetPot& input );
+    ~ErrorEstimatorOptions(){};
+
+    const std::string& estimator_type() const
+    { return _estimator_type; }
+
+    bool patch_reuse() const
+    { return _patch_reuse; }
+
+    unsigned char n_adjoint_h_refinements() const
+    { return _n_adjoint_h_refinements; }
+
+    unsigned char n_adjoint_p_refinements() const
+    { return _n_adjoint_p_refinements; }
+
+    bool compute_qoi_error_estimate() const
+    { return _compute_qoi_error_estimate; }
+
+  private:
+
+    void check_dup_input_style( const GetPot& input ) const;
+
+    bool is_old_style( const GetPot& input ) const;
+
+    void parse_old_style(const GetPot& input);
+
+    void parse_new_style(const GetPot& input);
+
+    std::string _estimator_type;
+    bool _patch_reuse;
+    unsigned char _n_adjoint_h_refinements;
+    unsigned char _n_adjoint_p_refinements;
+    bool _compute_qoi_error_estimate;
+
+  };
+
+} // end namespace GRINS
+
+#endif // GRINS_ERROR_ESTIMATOR_OPTIONS_H
