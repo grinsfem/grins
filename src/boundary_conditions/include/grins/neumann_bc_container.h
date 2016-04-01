87412a501ee4258edf1c761626187ecdc3fc00a1
Paul T. Bauman <ptbauman@gmail.com>
2016-04-01 15:15:10 -0400

    Add NeumannBCContainer

---
+++ src/boundary_conditions/include/grins/neumann_bc_container.h
@@ -0,0 +1,71 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef GRINS_NEUMANN_BC_CONTAINER_H
+#define GRINS_NEUMANN_BC_CONTAINER_H
+
+// GRINS
+#include "grins/var_typedefs.h"
+#include "grins/neumann_bc_abstract.h"
+#include "grins/shared_ptr.h"
+#include "grins/fe_variables_base.h"
+
+// libMesh
+#include "libmesh/auto_ptr.h" // UniquePtr
+
+namespace GRINS
+{
+  class NeumannBCContainer
+  {
+  public:
+    NeumannBCContainer( const std::set<BoundaryID>& bc_ids, const FEVariablesBase& fe_var,
+                        SharedPtr<NeumannBCAbstract>& func )
+      : _bc_ids(bc_ids),
+        _fe_var(fe_var),
+        _func(func)
+    {}
+
+    ~NeumannBCContainer(){};
+
+    bool has_bc_id( BoundaryID bc_id )
+    { return (_bc_ids.find(bc_id) != _bc_ids.end()); }
+
+    const FEVariablesBase& get_fe_var()
+    { return _fe_var; }
+
+    SharedPtr<NeumannBCAbstract>& get_func()
+    { return _func; }
+
+  private:
+
+    std::set<BoundaryID> _bc_ids;
+
+    const FEVariablesBase& _fe_var;
+
+    SharedPtr<NeumannBCAbstract> _func;
+  };
+
+} // end namespace GRINS
+
+#endif // GRINS_NEUMANN_BC_CONTAINER_H
