754b57dc14d5404186972985e85d4ff96c374130
Paul T. Bauman <ptbauman@gmail.com>
2016-05-03 14:41:02 -0400

    Add CatalycityFactoryAbstract
    This provides the groundwork to move to an abstract factory pattern
    for the construction of CatalycityBase subclasses.

---
+++ src/boundary_conditions/src/catalycity_factory_abstract.C
@@ -0,0 +1,47 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+// This class
+#include "grins/catalycity_factory_abstract.h"
+
+// Full specialization for the Factory<CatalycityBase>
+namespace libMesh
+{
+  template<>
+  std::map<std::string, libMesh::Factory<GRINS::CatalycityBase>*>&
+  libMesh::Factory<GRINS::CatalycityBase>::factory_map()
+  {
+    static std::map<std::string, libMesh::Factory<GRINS::CatalycityBase>*> _map;
+    return _map;
+  }
+} // end namespace libMesh
+
+// Definition of static members
+namespace GRINS
+{
+  template<>
+  const GetPot* FactoryWithGetPot<CatalycityBase>::_input = NULL;
+
+  std::string CatalycityFactoryAbstract::_section = std::string("DIE!");
+} // end namespace GRINS
