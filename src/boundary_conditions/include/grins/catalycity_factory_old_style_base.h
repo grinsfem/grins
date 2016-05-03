fc75dc01ea10a11d35b32fcd8f6368265dcb5719
Paul T. Bauman <ptbauman@gmail.com>
2016-05-03 15:13:32 -0400

    Add CatalycityFactoryOldStyleBase
    Add base class for handling construction of CatalycityBase objects
    using the "old style" of input parsing. The "OldStyle" nomenclature
    is a bit forward looking since the "new style" only exists downstream.

---
+++ src/boundary_conditions/include/grins/catalycity_factory_old_style_base.h
@@ -0,0 +1,70 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef GRINS_CATALYCITY_FACTORY_OLD_STYLE_BASE_H
+#define GRINS_CATALYCITY_FACTORY_OLD_STYLE_BASE_H
+
+// GRINS
+#include "grins/catalycity_factory_abstract.h"
+
+namespace GRINS
+{
+  class CatalycityFactoryOldStyleBase : public CatalycityFactoryAbstract
+  {
+  public:
+
+    CatalycityFactoryOldStyleBase( const std::string& physics_name )
+      : CatalycityFactoryAbstract(physics_name)
+    {}
+
+    ~CatalycityFactoryOldStyleBase(){};
+
+    static void set_reactant( const std::string& reactant )
+    { _reactant_str = reactant; }
+
+    static void set_bc_id( const std::string& bc_id )
+    { _bc_id_str = bc_id; }
+
+  protected:
+
+    virtual libMesh::UniquePtr<CatalycityBase> build_catalycity( const GetPot& input,
+                                                                 const std::string& section );
+
+    virtual libMesh::UniquePtr<CatalycityBase> build_catalycity_old_style( const GetPot& input,
+                                                                           const std::string& section,
+                                                                           const std::string& reactant_str,
+                                                                           const std::string& bc_id_string ) =0;
+    virtual void check_state() const;
+
+    virtual void reset_state();
+
+    static std::string _reactant_str;
+
+    static std::string _bc_id_str;
+
+  };
+
+} // end namespace GRINS
+
+#endif // GRINS_CATALYCITY_FACTORY_OLD_STYLE_BASE_H
---
+++ src/boundary_conditions/src/catalycity_factory_old_style_base.C
@@ -0,0 +1,60 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+// This class
+#include "grins/catalycity_factory_old_style_base.h"
+
+namespace GRINS
+{
+  libMesh::UniquePtr<CatalycityBase> CatalycityFactoryOldStyleBase::build_catalycity( const GetPot& input,
+                                                                                      const std::string& section )
+  {
+    // State of _reactant_str, _bc_id_str verified in check_state() call in create()
+    return this->build_catalycity_old_style( input, section, _reactant_str, _bc_id_str );
+  }
+
+  void CatalycityFactoryOldStyleBase::check_state() const
+  {
+    CatalycityFactoryAbstract::check_state();
+
+    if( _reactant_str == std::string("DIE!") )
+      libmesh_error_msg("ERROR: must call set_reactant() before building Catalycity!");
+
+    if( _bc_id_str == std::string("DIE!") )
+      libmesh_error_msg("ERROR: must call set_bc_id() before building Catalycity!");
+  }
+
+  void CatalycityFactoryOldStyleBase::reset_state()
+  {
+    CatalycityFactoryAbstract::reset_state();
+
+    _reactant_str = std::string("DIE!");
+    _bc_id_str = std::string("DIE!");
+  }
+
+  // Definition of static members
+  std::string CatalycityFactoryOldStyleBase::_reactant_str = std::string("DIE!");
+  std::string CatalycityFactoryOldStyleBase::_bc_id_str = std::string("DIE!");
+
+} // end namespace GRINS
