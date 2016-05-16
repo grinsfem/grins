71aef3ce0fc558daf27e4bc31495524c7b2d0caf
Paul T. Bauman <ptbauman@gmail.com>
2016-05-16 12:47:13 -0400

    Instantiate Gas*CatalyticWallNeumannBCOldStyleFactory

---
+++ src/boundary_conditions/src/gas_catalytic_wall_neumann_bc_old_style_factories.C
@@ -0,0 +1,37 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+// These classes
+#include "grins/gas_catalytic_wall_neumann_bc_old_style_factories.h"
+
+namespace GRINS
+{
+  // Instantiate and register Factory
+  GasRecombinationCatalyticWallNeumannBCOldStyleFactory
+  grins_factory_gas_recomb_catalytic_wall_neumann_bc_old_style("gas_recombination_catalytic_wall_old_style");
+
+  GasSolidCatalyticWallNeumannBCOldStyleFactory
+  grins_factory_gas_solid_catalytic_wall_neumann_bc_old_style("gas_solid_catalytic_wall_old_style");
+
+} // end namespace GRINS
