82d6ab2a94fae9321c5d540f46ccb64a569895cc
Paul T. Bauman <ptbauman@gmail.com>
2016-05-16 21:33:27 -0400

    Update regression/axi_thermally_driven_flow.sh test
    Test now uses generic_solution_regression

--- test/regression/axi_thermally_driven_flow.sh
+++ test/regression/axi_thermally_driven_flow.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-PROG="${GRINS_TEST_DIR}/test_thermally_driven_flow"
+PROG="${GRINS_TEST_DIR}/generic_solution_regression"
 
-INPUT="${GRINS_TEST_INPUT_DIR}/axi_thermally_driven_flow.in ${GRINS_TEST_DATA_DIR}/axi_thermally_driven.xdr"
+INPUT="${GRINS_TEST_INPUT_DIR}/axi_thermally_driven_flow.in"
+DATA="${GRINS_TEST_DATA_DIR}/axi_thermally_driven.xdr"
 
-${LIBMESH_RUN:-} $PROG $INPUT
+${LIBMESH_RUN:-} $PROG input=$INPUT soln-data=$DATA vars='u v p T' norms='L2 H1' tol='8.0e-9'
