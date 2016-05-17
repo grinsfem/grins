d1fe2df74b39ea993cb97318fb4725f721bc9bf4
Paul T. Bauman <ptbauman@gmail.com>
2016-05-16 21:29:24 -0400

    Update regression/thermally_driven_3d_flow.sh test
    Test now uses generic_solution_regression

--- test/regression/thermally_driven_3d_flow.sh
+++ test/regression/thermally_driven_3d_flow.sh
@@ -2,13 +2,12 @@
 
 if [ "${GRINS_LIBMESH_DIM}" -gt 2 ]
 then
-   PROG="${GRINS_TEST_DIR}/test_thermally_driven_flow"
+   PROG="${GRINS_TEST_DIR}/generic_solution_regression"
 
-   INPUT="${GRINS_TEST_INPUT_DIR}/thermally_driven_3d_flow.in ${GRINS_TEST_DATA_DIR}/thermally_driven_3d.xdr"
+   INPUT="${GRINS_TEST_INPUT_DIR}/thermally_driven_3d_flow.in"
+   DATA="${GRINS_TEST_DATA_DIR}/thermally_driven_3d.xdr"
 
-   LIBMESH_OPTIONS="--n_threads=6"
-
-   ${LIBMESH_RUN:-} $PROG $INPUT
+   ${LIBMESH_RUN:-} $PROG input=$INPUT soln-data=$DATA vars='u v p T' norms='L2 H1' tol='8.0e-9'
 else
    # If LIBMESH_DIM != 3, then we skip this test
    exit 77
