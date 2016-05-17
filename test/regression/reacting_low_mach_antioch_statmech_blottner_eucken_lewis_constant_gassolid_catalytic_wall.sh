004a38a0574dd0d5eb76ac1d2b590492a9e2302f
Paul T. Bauman <ptbauman@gmail.com>
2016-05-17 00:21:46 -0400

    Move reacting_low_mach tests to generic_solution_regression
    Had to put initial conditions in the input file and update
    the .sh files.

--- test/regression/reacting_low_mach_antioch_statmech_blottner_eucken_lewis_constant_gassolid_catalytic_wall.sh
+++ test/regression/reacting_low_mach_antioch_statmech_blottner_eucken_lewis_constant_gassolid_catalytic_wall.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-PROG="${GRINS_TEST_DIR}/reacting_low_mach_regression"
+PROG="${GRINS_TEST_DIR}/generic_solution_regression"
 
-INPUT="${GRINS_TEST_INPUT_DIR}/reacting_low_mach_antioch_statmech_blottner_eucken_lewis_constant_gassolid_catalytic_wall_regression.in ${GRINS_TEST_DATA_DIR}/reacting_low_mach_antioch_statmech_blottner_eucken_lewis_constant_gassolid_catalytic_wall_regression.xdr"
+INPUT="${GRINS_TEST_INPUT_DIR}/reacting_low_mach_antioch_statmech_blottner_eucken_lewis_constant_gassolid_catalytic_wall_regression.in"
+DATA="${GRINS_TEST_DATA_DIR}/reacting_low_mach_antioch_statmech_blottner_eucken_lewis_constant_gassolid_catalytic_wall_regression.xdr"
 
 PETSC_OPTIONS="-pc_factor_levels 4 -sub_pc_factor_levels 4"
 
-${LIBMESH_RUN:-} $PROG $INPUT $PETSC_OPTIONS 
+${LIBMESH_RUN:-} $PROG input=$INPUT soln-data=$DATA vars='u v T p w_N2 w_N' norms='L2 H1' tol='1.5e-8' $PETSC_OPTIONS
