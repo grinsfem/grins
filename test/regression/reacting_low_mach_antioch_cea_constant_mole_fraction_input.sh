03690a020351b8ba40760f23ba48e4af1ca6a112
Paul T. Bauman <ptbauman@gmail.com>
2016-05-17 21:18:40 -0400

    Add regression/reacting_low_mach_antioch_cea_constant_mole_fraction_input.sh test
    This is identical to regression/reacting_low_mach_antioch_cea_constant.sh
    except the boundary conditions are specified using mole fractions instead
    of mass fractions. This is, thus, excercising this boundary condition
    specification.

---
+++ test/regression/reacting_low_mach_antioch_cea_constant_mole_fraction_input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PROG="${GRINS_TEST_DIR}/generic_solution_regression"
+
+INPUT="${GRINS_TEST_INPUT_DIR}/reacting_low_mach_antioch_cea_constant_mole_fraction_input_regression.in"
+
+# The mole fractions input should produce the same solution as the mass fraction case
+DATA="${GRINS_TEST_DATA_DIR}/reacting_low_mach_antioch_cea_constant_regression.xdr"
+
+# A MOAB preconditioner
+PETSC_OPTIONS="-pc_type asm -pc_asm_overlap 10 -sub_pc_type ilu -sub_pc_factor_shift_type nonzero -sub_pc_factor_levels 10"
+
+${LIBMESH_RUN:-} $PROG input=$INPUT soln-data=$DATA vars='u v T p w_N2 w_N' norms='L2 H1' tol='1.5e-8' $PETSC_OPTIONS
