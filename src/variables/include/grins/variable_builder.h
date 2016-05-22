c472c4624c8969d899980dc234f7d20ec34d6821
Paul T. Bauman <ptbauman@gmail.com>
2016-05-22 18:23:09 -0400

    Add VariableBuilder
    This provides the static function to call to build the variables as
    well as the interface, and some common functions, for the actual
    construction of FEVariablesBase objects.

---
+++ src/variables/include/grins/variable_builder.h
@@ -0,0 +1,79 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef GRINS_VARIABLE_BUILDER_H
+#define GRINS_VARIABLE_BUILDER_H
+
+// GRINS
+#include "grins/shared_ptr.h"
+#include "grins/fe_variables_base.h"
+
+// libMesh
+#include "libmesh/auto_ptr.h" // libMesh::UniquePtr
+
+// libMesh forward declarations
+class GetPot;
+
+namespace GRINS
+{
+  // Forward declarations
+  class MultiphysicsSystem;
+
+  //! Manages runtime construction of the FEVariableBase objects
+  /*! build_variables() is intended to be called before building the
+      Physics and boundary conditions so that the VariableWarhouse is
+      populated and can be referenced by the Physics and boundary conditions. */
+  class VariableBuilder
+  {
+  public:
+
+    VariableBuilder(){}
+    ~VariableBuilder(){}
+
+    static void build_variables( const GetPot& input,
+                                 MultiphysicsSystem& system );
+
+    //! Implementation of Variable construction done in subclasses
+    virtual void build_variables_impl( const GetPot& input,
+                                       MultiphysicsSystem& system ) =0;
+
+  protected:
+
+    void add_variable_to_warehouse( SharedPtr<FEVariablesBase>& fe_var,
+                                    const std::string& var_name );
+
+    void add_vars_to_system( MultiphysicsSystem& system,
+                             const std::vector<std::string>& var_names,
+                             const std::string& fe_family,
+                             const std::string& order,
+                             std::vector<VariableIndex>& var_indices );
+
+    SharedPtr<FEVariablesBase> build_fe_var( const std::string& var_type,
+                                             const std::vector<std::string>& var_names,
+                                             const std::vector<VariableIndex>&  var_indices );
+
+  };
+} // end namespace GRINS
+
+#endif // GRINS_VARIABLE_BUILDER_H
---
+++ src/variables/src/variable_builder.C
@@ -0,0 +1,85 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+// This class
+#include "grins/variable_builder.h"
+
+// GRINS
+#include "grins/default_variable_builder.h"
+#include "grins/multiphysics_sys.h"
+#include "grins/variable_factory.h"
+
+// libMesh
+#include "libmesh/getpot.h"
+#include "libmesh/string_to_enum.h"
+
+namespace GRINS
+{
+  void VariableBuilder::build_variables( const GetPot& input,
+                                         MultiphysicsSystem& system )
+  {
+    DefaultVariableBuilder var_builder;
+
+    // Defer the construction to the builder subclass implementation
+    var_builder.build_variables_impl(input,system);
+  }
+
+  void VariableBuilder::add_variable_to_warehouse( SharedPtr<FEVariablesBase>& fe_var,
+                                                   const std::string& var_name )
+  {
+    libmesh_not_implemented();
+  }
+
+  void VariableBuilder::add_vars_to_system( MultiphysicsSystem& system,
+                                            const std::vector<std::string>& var_names,
+                                            const std::string& fe_family,
+                                            const std::string& order,
+                                            std::vector<VariableIndex>& var_indices )
+  {
+    const unsigned int n_vars = var_names.size();
+
+    // Setup var_indices
+    libmesh_assert( var_indices.empty() );
+    var_indices.resize(n_vars);
+
+    for( unsigned int v = 0; v < n_vars; v++ )
+      var_indices[v] = system.add_variable( var_names[v],
+                                            libMesh::Utility::string_to_enum<GRINSEnums::Order>(order),
+                                            libMesh::Utility::string_to_enum<GRINSEnums::FEFamily>(fe_family) );
+  }
+
+  SharedPtr<FEVariablesBase> VariableBuilder::build_fe_var( const std::string& var_type,
+                                                            const std::vector<std::string>& var_names,
+                                                            const std::vector<VariableIndex>&  var_indices )
+  {
+    // Setup VariableFactory
+    VariableFactoryAbstract::set_var_names(var_names);
+    VariableFactoryAbstract::set_var_indices(var_indices);
+
+    libMesh::UniquePtr<FEVariablesBase> var = VariableFactoryAbstract::build(var_type);
+
+    // Need to return a SharedPtr, so release from the UniquePtr we got back
+    return SharedPtr<FEVariablesBase>( var.release() );
+  }
+} // end namespace GRINS
