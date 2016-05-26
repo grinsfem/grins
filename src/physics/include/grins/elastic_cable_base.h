b60652fa17c29142e7e363b23d26c44be245dce8
Paul T. Bauman <ptbauman@gmail.com>
2016-05-26 16:24:36 -0400

    For real, it's 2016

--- src/physics/include/grins/elastic_cable_base.h
+++ src/physics/include/grins/elastic_cable_base.h
@@ -3,7 +3,7 @@
 //
 // GRINS - General Reacting Incompressible Navier-Stokes
 //
-// Copyright (C) 2014-2015 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2014-2016 Paul T. Bauman, Roy H. Stogner
 // Copyright (C) 2010-2013 The PECOS Development Team
 //
 // This library is free software; you can redistribute it and/or
