b17f0177e48129958aef416dad9d1ab6cac9c19d
Paul T. Bauman <ptbauman@gmail.com>
2016-05-26 14:47:54 -0400

    Add PETSC_OPTIONS to regression/parsed_qoi_scalar.sh test
    This test is effectively identical to its parsed_qoi.sh cousin,
    it just has a few extra qois. So, use the same solver options so
    we can successfully run the test in parallel.

--- test/regression/parsed_qoi_scalar.sh
+++ test/regression/parsed_qoi_scalar.sh
@@ -4,5 +4,7 @@ PROG="${GRINS_BUILDSRC_DIR}/grins"
 
 INPUT="${GRINS_TEST_INPUT_DIR}/parsed_qoi.in"
 
-${LIBMESH_RUN:-} $PROG $INPUT
+PETSC_OPTIONS="-pc_type asm -pc_asm_overlap 4 -sub_pc_type ilu -sub_pc_factor_levels 4 -sub_pc_factor_mat_ordering_type 1wd -sub_pc_factor_shift_type nonzero"
+
+${LIBMESH_RUN:-} $PROG $INPUT $PETSC_OPTIONS
 
