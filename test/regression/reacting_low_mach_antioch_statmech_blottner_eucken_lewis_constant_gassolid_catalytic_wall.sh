64ac0145e817c59fa357565988e1a5d294914814
Paul T. Bauman <ptbauman@gmail.com>
2016-05-31 10:13:37 -0400

    Skip Antioch regression test when Antioch not present
    This takes care of #410.

--- test/regression/reacting_low_mach_antioch_statmech_blottner_eucken_lewis_constant_gassolid_catalytic_wall.sh
+++ test/regression/reacting_low_mach_antioch_statmech_blottner_eucken_lewis_constant_gassolid_catalytic_wall.sh
@@ -7,4 +7,8 @@ DATA="${GRINS_TEST_DATA_DIR}/reacting_low_mach_antioch_statmech_blottner_eucken_
 
 PETSC_OPTIONS="-pc_factor_levels 4 -sub_pc_factor_levels 4"
 
-${LIBMESH_RUN:-} $PROG input=$INPUT soln-data=$DATA vars='u v T p w_N2 w_N' norms='L2 H1' tol='1.5e-8' $PETSC_OPTIONS
+if [ $GRINS_ANTIOCH_ENABLED == 1 ]; then
+   ${LIBMESH_RUN:-} $PROG input=$INPUT soln-data=$DATA vars='u v T p w_N2 w_N' norms='L2 H1' tol='1.5e-8' $PETSC_OPTIONS
+else
+   exit 77;
+fi
