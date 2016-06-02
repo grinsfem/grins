59c1876f081e7cd1125ceee367ceb10981e97ad3
Paul T. Bauman <ptbauman@gmail.com>
2016-06-02 12:30:09 -0400

    Add optional second argument for parallel loop test script
    This way we can restrict the TESTS that it runs when going through
    all the processor variations.

--- test/run_tests_parallel_loop.sh
+++ test/run_tests_parallel_loop.sh
@@ -8,6 +8,13 @@
 # error if neither is specified. Note that the -j argument to check is halved
 # each time and starts at the max.
 #
+# An optional second argument is allowed. If second argument is present,
+# then all tests are run each time. If you only want to run a subset of tests,
+# then supply the argument TESTS="<space separated list of tests>" (exactly
+# as you supply to normal make check). For example, if we only wanted to run the
+# following two tests for each pass, supply the following as the second argument:
+# TESTS="regression/thermally_driven_2d_flow.sh regression/reacting_low_mach_antioch_statmech_constant.sh"
+#
 # The output goes GRINS_TESTING_OUTPUT_FILE if it's set in the environment
 # or to parallel_loop_check_tests.log if it's not. A handy command for parsing
 # the output is:
@@ -25,9 +32,13 @@ if [ -n $1 ]; then
    export GRINS_TESTING_MAX_N_PROCS_VALUE=$1
 fi
 
+if [ -n $2 ]; then
+   export GRINS_TESTING_TESTS_SUBSET=$2
+fi
+
 for i in $(seq 1 ${GRINS_TESTING_MAX_N_PROCS_VALUE:?undefined}); \
    do export LIBMESH_RUN="mpiexec -np $i"; \
    j=$((${GRINS_TESTING_MAX_N_PROCS_VALUE:?undefined}/$i)); \
    echo LIBMESH_RUN is $LIBMESH_RUN | tee -a ${GRINS_TESTING_OUTPUT_VALUE}; \
-   make -j $j check 2>&1 | tee -a ${GRINS_TESTING_OUTPUT_VALUE}; \
+   make -j $j $GRINS_TESTING_TESTS_SUBSET check 2>&1 | tee -a ${GRINS_TESTING_OUTPUT_VALUE}; \
 done
