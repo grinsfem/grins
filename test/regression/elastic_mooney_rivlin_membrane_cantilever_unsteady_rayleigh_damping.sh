6d1ad74aa4451067ec6ed15c2858930365c2b6d4
Paul T. Bauman <ptbauman@gmail.com>
2016-06-17 13:27:16 -0400

    Add 2D membrane dynamics+damping regression test
    This exercises the 2D-only capability of ElasticMembrane dynamics
    and the ElasticMembraneRayleighDamping.

---
+++ test/regression/elastic_mooney_rivlin_membrane_cantilever_unsteady_rayleigh_damping.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PROG="${GRINS_TEST_DIR}/generic_solution_regression"
+
+INPUT="${GRINS_TEST_INPUT_DIR}/elastic_mooney_rivlin_membrane_cantilever_unsteady_rayleigh_damping_regression.in"
+DATA="${GRINS_TEST_DATA_DIR}/elastic_mooney_rivlin_membrane_cantilever_unsteady_rayleigh_damping_regression.xdr"
+
+PETSC_OPTIONS="-ksp_type cg -pc_type bjacobi -sub_pc_type icc"
+
+${LIBMESH_RUN:-} $PROG $PETSC_OPTIONS input=$INPUT soln-data=$DATA vars='Ux Uy' norms='L2 H1' tol='1.0e-10'
