49943eaed89a0a32eab5d9aca6d7bb24919c0412
Paul T. Bauman <ptbauman@gmail.com>
2016-06-17 15:26:01 -0400

    Update suspended_cable regression
    Now only uses two displacement variables and uses the generic
    regression test. Also removed now unneeded program for suspended
    cable regression.

--- test/regression/suspended_cable_test.sh
+++ test/regression/suspended_cable_test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-PROG="${GRINS_TEST_DIR}/suspended_cable_regression"
+PROG="${GRINS_TEST_DIR}/generic_solution_regression"
 
-INPUT="${GRINS_TEST_INPUT_DIR}/suspended_cable_test.in ${GRINS_TEST_DATA_DIR}/suspended_cable_test.xdr"
+INPUT="${GRINS_TEST_INPUT_DIR}/suspended_cable_test.in"
+DATA="${GRINS_TEST_DATA_DIR}/suspended_cable_test.xdr"
 
-${LIBMESH_RUN:-} $PROG $INPUT
+PETSC_OPTIONS="-ksp_type cg -pc_type bjacobi -sub_pc_type icc -sub_pc_factor_shift_type nonzero -ksp_converged_reason"
+
+${LIBMESH_RUN:-} $PROG input=$INPUT soln-data=$DATA vars='Ux Uy' norms='L2 H1' tol='1.0e-10' $PETSC_OPTIONS
