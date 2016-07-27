d0b5d2da2127cead762d125f01cbd3c7ad5cc5b1
Paul T. Bauman <ptbauman@gmail.com>
2016-07-27 09:01:49 -0400

    Update gold data for libMesh/libmesh#1040
    Test is regression/extra_quadrature_order_laplace_arefee_amr.sh
    The refinement pattern is changed slightly by the libMesh PR. Thus,
    we need to generate new gold data. This test will now *fail* with
    libMesh before PR #1040.

--- test/regression/extra_quadrature_order_laplace_arefee_amr.sh
+++ test/regression/extra_quadrature_order_laplace_arefee_amr.sh
@@ -5,4 +5,4 @@ PROG="${GRINS_TEST_DIR}/generic_solution_regression"
 INPUT="${GRINS_TEST_INPUT_DIR}/extra_quadrature_order_laplace_arefee_amr_regression.in"
 DATA="${GRINS_TEST_DATA_DIR}/extra_quadrature_order_laplace_arefee_amr_regression.xdr"
 
-${LIBMESH_RUN:-} $PROG input=$INPUT soln-data=$DATA vars='T' norms='L2 H1' tol='1.0e-10' qois='-3.3293173857546449e+01'
+${LIBMESH_RUN:-} $PROG input=$INPUT soln-data=$DATA vars='T' norms='L2 H1' tol='1.0e-10' qois='-3.3293172601959668e+01'
