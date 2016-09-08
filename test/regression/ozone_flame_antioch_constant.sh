0887b05e35dc8b1d720091c999340b1861703501
Roy H. Stogner <roystgnr@ices.utexas.edu>
2016-09-07 20:41:12 -0500

    Skip antioch test when configured without Antioch

--- test/regression/ozone_flame_antioch_constant.sh
+++ test/regression/ozone_flame_antioch_constant.sh
@@ -7,4 +7,8 @@ DATA="${GRINS_TEST_DATA_DIR}/ozone_flame_antioch_constant_regression.xdr"
 
 PETSC_OPTIONS="-ksp_type gmres -pc_type bjacobi -sub_pc_type lu -sub_pc_factor_shift_type nonzero"
 
-${LIBMESH_RUN:-} $PROG input=$INPUT soln-data=$DATA vars='Ux Uy T p Y_O Y_O2 Y_O3' norms='L2 H1' tol='1.0e-6' $PETSC_OPTIONS
+if [ $GRINS_ANTIOCH_ENABLED == 1 ]; then
+   ${LIBMESH_RUN:-} $PROG input=$INPUT soln-data=$DATA vars='Ux Uy T p Y_O Y_O2 Y_O3' norms='L2 H1' tol='1.0e-6' $PETSC_OPTIONS
+else
+   exit 77;
+fi
