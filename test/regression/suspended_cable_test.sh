83b9d706b4f00a52fae29d2e9033474f5fc6e257
Paul T. Bauman <ptbauman@gmail.com>
2016-09-14 11:49:42 -0400

    Update suspended_cable_test options
    Cholesky sub_pc is not robust in parallel as the PC dies due
    to indefiniteness using pc bjacobi. So, be gross and just use
    GMRES with LU. This runs on 1-16 processors for me.

--- test/regression/suspended_cable_test.sh
+++ test/regression/suspended_cable_test.sh
@@ -5,6 +5,6 @@ PROG="${GRINS_TEST_DIR}/generic_solution_regression"
 INPUT="${GRINS_TEST_INPUT_DIR}/suspended_cable_test.in"
 DATA="${GRINS_TEST_DATA_DIR}/suspended_cable_test.xdr"
 
-PETSC_OPTIONS="-ksp_type cg -pc_type bjacobi -sub_pc_type icc -sub_pc_factor_shift_type nonzero -ksp_converged_reason"
+PETSC_OPTIONS="-ksp_type gmres -pc_type bjacobi -sub_pc_type lu -sub_pc_factor_shift_type nonzero -ksp_converged_reason"
 
 ${LIBMESH_RUN:-} $PROG input=$INPUT soln-data=$DATA vars='Ux Uy' norms='L2 H1' tol='1.0e-10' $PETSC_OPTIONS
