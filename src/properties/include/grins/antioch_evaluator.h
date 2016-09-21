c689dcfe6ec2ea85d50925dbedcb0cf88d9252d6
Roy H. Stogner <roystgnr@ices.utexas.edu>
2016-09-21 13:02:17 -0500

    Never pass negative temperature to Antioch

--- src/properties/include/grins/antioch_evaluator.h
+++ src/properties/include/grins/antioch_evaluator.h
@@ -99,6 +99,10 @@ namespace GRINS
 
     libMesh::UniquePtr<AntiochKinetics> _kinetics;
 
+    // Temperature should be clipped to positive values for
+    // stability's sake
+    libMesh::Real _clipped_T;
+
     libMesh::UniquePtr<Antioch::TempCache<libMesh::Real> > _temp_cache;
 
     //! Helper method for managing _temp_cache
@@ -198,10 +202,12 @@ namespace GRINS
   inline
   void AntiochEvaluator<Thermo>::check_and_reset_temp_cache( const libMesh::Real& T )
   {
+    _clipped_T = std::max(libMesh::Real(0), T);
+
     // We can't compare T because it's a reference, so we may have already
     // changed it upstream. So, we compare the next cheapest thing.
-    if( _temp_cache->T2 != T*T )
-      _temp_cache.reset( new Antioch::TempCache<libMesh::Real>(T) );
+    if( _temp_cache->T2 != _clipped_T*_clipped_T )
+      _temp_cache.reset( new Antioch::TempCache<libMesh::Real>(_clipped_T) );
   }
 
 } // end namespace GRINS
