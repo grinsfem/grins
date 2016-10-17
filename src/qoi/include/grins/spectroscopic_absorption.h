56dd753ad473265c8387766c03e929b453e9ed8d
Tim Adowski <tradowsk@buffalo.edu>
2016-10-17 13:20:37 -0400

    add SpectroscopicAbsorption class

---
+++ src/qoi/include/grins/spectroscopic_absorption.h
@@ -0,0 +1,71 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2016 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+
+#ifndef GRINS_SPECTROSCOPIC_ABSORPTION_H
+#define GRINS_SPECTROSCOPIC_ABSORPTION_H
+
+// GRINS
+#include "grins/integrated_function.h"
+#include "grins/absorption_coeff.h"
+#include "grins/single_variable.h"
+
+namespace GRINS
+{
+  /*!
+    QoI class for absorption calculations using the Beer-Lambert Law
+
+    Relies upon the IntegratedFunction class for hooking into QoI infrastructure,
+    and AbsorptionCoeff class for evaluating the <i>spectral absorption coefficient</i>, \f$ k_{\nu} \f$
+    
+    \f$ \frac{I_{\nu}}{I_{\nu}^0} = \exp\left\{- \int_0^L k_{\nu} dx\right\} \f$
+    
+    where \f$ \frac{I_{\nu}}{I_{\nu}^0} \f$ is the <i>spectral absorption</i>
+
+    Expects all parameters given in standard SI units [m], [K], [Pa]
+  */
+  class SpectroscopicAbsorption : public IntegratedFunction<libMesh::FEMFunctionBase<libMesh::Real> >
+  {
+  public:
+
+    /*!
+      @param absorb AbsorptionCoeff object
+      @param rayfire Uninitialized RayfireMesh object
+    */
+    SpectroscopicAbsorption( const std::string & qoi_name, SharedPtr<libMesh::FEMFunctionBase<libMesh::Real> > absorb, SharedPtr<RayfireMesh> rayfire );
+
+    virtual QoIBase * clone() const;
+
+    //! Override the QoIBase implementation to perform exp(-kv*L)
+    virtual void parallel_op( const libMesh::Parallel::Communicator & communicator,
+                                    libMesh::Number & sys_qoi,
+		                                libMesh::Number & local_qoi );
+
+  private:
+
+    SpectroscopicAbsorption();
+
+  };
+}
+#endif //GRINS_SPECTROSCOPIC_ABSORPTION_H
---
+++ src/qoi/src/spectroscopic_absorption.C
@@ -0,0 +1,66 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2016 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+
+// This class
+#include "grins/spectroscopic_absorption.h"
+#include "grins/absorption_coeff.h"
+#include "grins/integrated_function.h"
+
+// GRINS
+#include "grins/multiphysics_sys.h"
+#include "grins/assembly_context.h"
+#include "grins/materials_parsing.h"
+#include "grins/single_variable.h"
+
+// libMesh
+#include "libmesh/getpot.h"
+#include "libmesh/fem_system.h"
+#include "libmesh/quadrature.h"
+#include "libmesh/fem_function_base.h"
+
+namespace GRINS
+{
+  SpectroscopicAbsorption::SpectroscopicAbsorption( const std::string & qoi_name, SharedPtr<libMesh::FEMFunctionBase<libMesh::Real> > absorb, SharedPtr<RayfireMesh> rayfire)
+    : IntegratedFunction<libMesh::FEMFunctionBase<libMesh::Real> >(2 /* QGauss order */,absorb,rayfire,qoi_name)
+  {}
+
+  QoIBase * SpectroscopicAbsorption::clone() const
+  {
+    return new SpectroscopicAbsorption( *this );
+  }
+
+  void SpectroscopicAbsorption::parallel_op( const libMesh::Parallel::Communicator & communicator,
+                                            libMesh::Number & sys_qoi,
+	                                        libMesh::Number & local_qoi )
+  {
+    QoIBase::parallel_op(communicator,sys_qoi,local_qoi);
+
+    // absorption coefficient is calculated in [cm^-1], but path length is given in [m]
+    // 100.0 factor converts pathlength to [cm]
+    sys_qoi = std::exp( -sys_qoi * 100.0 );
+    QoIBase::_qoi_value = sys_qoi;
+  }
+
+} //namespace GRINS
