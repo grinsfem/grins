cb96f377c23333c2fc4ddf14eaa0e74b0f19338a
Roy H. Stogner <roystgnr@ices.utexas.edu>
2016-12-19 16:58:22 -0600

    Test command line overrides
    I change the input file to use the wrong size mesh, and then change
    back to the right size mesh via the command line.  If the command line
    arguments are removed (or, I hope, if they're misparsed) then the test
    will now fail.

--- test/exact_soln/poisson_periodic_2d_x.sh
+++ test/exact_soln/poisson_periodic_2d_x.sh
@@ -7,11 +7,22 @@ TESTDATA="./poisson_periodic_2d_x.xda"
 
 PETSC_OPTIONS="-ksp_type cg -pc_type bjacobi -sub_pc_type icc"
 
-# First run the case with grins
-${LIBMESH_RUN:-} ${GRINS_BUILDSRC_DIR}/grins $INPUT $PETSC_OPTIONS
+# First run the case with grins.  We'll override part of the input
+# file on the command line, too, to test that feature.
+${LIBMESH_RUN:-} ${GRINS_BUILDSRC_DIR}/grins \
+  $INPUT \
+  Mesh/Generation/n_elems_x=20 \
+  Mesh/Generation/n_elems_y=20 \
+  $PETSC_OPTIONS
 
 # Now run the test part to make sure we're getting the correct thing
-${LIBMESH_RUN:-} ${GRINS_TEST_DIR}/generic_exact_solution_testing_app input=$INPUT vars='u' norms='L2' tol='1.0e-8' u_L2_error='0.00349104' u_exact_soln='sin(pi*y)*cos(2*pi*x)' test_data=$TESTDATA
+${LIBMESH_RUN:-} ${GRINS_TEST_DIR}/generic_exact_solution_testing_app \
+  input=$INPUT \
+  Mesh/Generation/n_elems_x=20 \
+  Mesh/Generation/n_elems_y=20 \
+  vars='u' norms='L2' tol='1.0e-8' \
+  u_L2_error='0.00349104' u_exact_soln='sin(pi*y)*cos(2*pi*x)' \
+  test_data=$TESTDATA
 
 # Now remove the test turd
 rm $TESTDATA
