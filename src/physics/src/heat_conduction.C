0245565794fce7eaa0875253cb5fdbfb25d6d09e
Paul T. Bauman <ptbauman@gmail.com>
2017-03-01 14:11:52 -0500

    Update time_evolving API for all first order Physics

--- src/physics/src/heat_conduction.C
+++ src/physics/src/heat_conduction.C
@@ -64,7 +64,7 @@ namespace GRINS
   void HeatConduction<K>::set_time_evolving_vars( libMesh::FEMSystem* system )
   {
     // Tell the system to march temperature forward in time
-    system->time_evolving(_temp_vars.T(), 1);
+    system->time_evolving(_temp_vars.T(),1);
   }
 
   template<class K>
