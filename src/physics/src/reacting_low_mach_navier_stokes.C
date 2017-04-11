9c00757b39e6c471b0a990cf4235f98e5c27216f
Paul T. Bauman <ptbauman@gmail.com>
2017-04-11 11:17:20 -0400

    Update thermal conductivity output in RLMNS
    Although using this function is not very efficient, it is implemented
    for all the types we want to use, so we can get output for the newer and
    more complex transport models. And since this is mostly for debugging
    anyways, I'm not super concerned with efficiency at this point.

--- src/physics/src/reacting_low_mach_navier_stokes.C
+++ src/physics/src/reacting_low_mach_navier_stokes.C
@@ -701,11 +701,22 @@ namespace GRINS
     else if( quantity_index == this->_k_index )
       {
         std::vector<libMesh::Real> Y( this->_n_species );
+
         libMesh::Real T = this->T(point,context);
         this->mass_fractions( point, context, Y );
         libMesh::Real p0 = this->get_p0_steady(context,point);
 
-        value = gas_evaluator.k( T, p0, Y );
+        libMesh::Real cp = gas_evaluator.cp( T, p0, Y );
+
+        libMesh::Real rho = this->rho( T, p0, gas_evaluator.R_mix(Y) );
+
+        libMesh::Real mu, k;
+        std::vector<libMesh::Real> D( this->_n_species );
+
+        gas_evaluator.mu_and_k_and_D( T, rho, cp, Y, mu, k, D );
+
+        value = k;
+        return;
       }
     else if( quantity_index == this->_cp_index )
       {
