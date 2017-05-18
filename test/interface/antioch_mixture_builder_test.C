fb65bb904239d996aaddbe2487b8725911193b9c
Paul T. Bauman <ptbauman@gmail.com>
2017-05-18 13:54:48 -0400

    Add AntiochMixtureBuilderBaseTest unit testing
    Staring with test_build_chem_mix. Only basic tests for now. We
    can expand later as we introduce more file formats, etc.

---
+++ test/interface/antioch_mixture_builder_test.C
@@ -0,0 +1,83 @@
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2016 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#include "grins_config.h"
+
+#ifdef GRINS_HAVE_CPPUNIT
+#ifdef GRINS_HAVE_ANTIOCH
+
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/TestCase.h>
+
+// Testing
+#include "grins_test_paths.h"
+
+//GRINS
+#include "grins/antioch_mixture_builder_base.h"
+
+namespace GRINSTesting
+{
+  class AntiochMixtureBuilderBaseTest : public CppUnit::TestCase
+  {
+  public:
+    CPPUNIT_TEST_SUITE( AntiochMixtureBuilderBaseTest );
+
+    CPPUNIT_TEST( test_build_chem_mix );
+
+    CPPUNIT_TEST_SUITE_END();
+
+  public:
+
+    void setUp()
+    {
+      std::string input_file = std::string(GRINS_TEST_SRCDIR)+"/input_files/antioch.in";
+      _input.reset( new GetPot(input_file) );
+    }
+
+    void test_build_chem_mix()
+    {
+      GRINS::AntiochMixtureBuilderBase builder;
+
+      libMesh::UniquePtr<Antioch::ChemicalMixture<libMesh::Real> > chem_mix
+        = builder.build_chem_mix(*_input, "TestMaterial");
+
+      // Just a couple of basic tests for the parsed ChemicalMixture
+      CPPUNIT_ASSERT_EQUAL( 5, (int)chem_mix->n_species() );
+
+      const std::vector<Antioch::ChemicalSpecies<libMesh::Real>*> & all_species =
+        chem_mix->chemical_species();
+
+      CPPUNIT_ASSERT_EQUAL( 5, (int)all_species.size() );
+    }
+
+  private:
+
+    libMesh::UniquePtr<GetPot> _input;
+  };
+
+  CPPUNIT_TEST_SUITE_REGISTRATION( AntiochMixtureBuilderBaseTest );
+
+} // end namespace GRINSTesting
+
+#endif // GRINS_HAVE_ANTIOCH
+#endif // GRINS_HAVE_CPPUNIT
