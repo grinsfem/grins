48b6d2a56393ec55acafd25aa07d9cd50b2b38aa
Paul T. Bauman <ptbauman@gmail.com>
2017-06-06 09:33:31 -0400

    Add ChemistryBuilder
    We'll use this when we need to builder either an AntiochChemistry
    or a CanteraMixture object.

---
+++ src/properties/include/grins/chemistry_builder.h
@@ -0,0 +1,51 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2016 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+
+#ifndef GRINS_CHEMISTRY_BUILDER_H
+#define GRINS_CHEMISTRY_BUILDER_H
+
+// libMesh
+#include "libmesh/auto_ptr.h" // libMesh::UniquePtr
+#include "libmesh/getpot.h"
+
+// C++
+#include <string>
+
+namespace GRINS
+{
+  class ChemistryBuilder
+  {
+  public:
+    ChemistryBuilder(){}
+    ~ChemistryBuilder(){}
+
+    template<typename ChemistryType>
+    void build_chemistry( const GetPot & input,const std::string & material,
+                          libMesh::UniquePtr<ChemistryType> & chem_ptr );
+  };
+
+} // end namespace GRINS
+
+#endif // GRINS_CHEMISTRY_BUILDER_H
