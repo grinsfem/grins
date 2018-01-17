d5055256a47a953b419dbdeaa807c008e4f98abc
Kenneth Budzinski <klbudzin@buffalo.edu>
2018-01-17 11:25:34 -0500

    adding the instantiating file

---
+++ src/physics/src/od_premixed_flame_instantiate.C
@@ -0,0 +1,53 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2017 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+
+#include "grins_config.h"
+
+#include "grins/od_premixed_flame.h"
+#include "od_premixed_flame.C"
+
+#ifdef GRINS_HAVE_CANTERA
+
+#include "grins/cantera_mixture.h"
+#include "grins/cantera_evaluator.h"
+
+
+template class GRINS::ODPremixedFlame<GRINS::CanteraMixture,GRINS::CanteraEvaluator>;
+
+#endif // GRINS_HAVE_CANTERA
+
+
+#ifdef GRINS_HAVE_ANTIOCH
+
+#include "grins/reacting_low_mach_navier_stokes_macro.h"
+
+
+INSTANTIATE_REACTING_LOW_MACH_SUBCLASS_CONSTANT_MIXTURE_AND_CONSTANT_EVALUATOR(ODPremixedFlame);
+
+
+INSTANTIATE_REACTING_LOW_MACH_SUBCLASS_MIXTURE_AND_EVALUATOR(ODPremixedFlame);
+
+
+#endif //GRINS_HAVE_ANTIOCH
