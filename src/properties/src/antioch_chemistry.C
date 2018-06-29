22ee02ff6b3c5ca7d44ca7d3e88c2ba5ed2479cb
Paul T. Bauman <ptbauman@gmail.com>
2018-06-29 14:38:46 -0400

    Consolidate Antioch::ChemMixture construction
    Use the builder since we have it instead of keeping copied code
    around.

--- src/properties/src/antioch_chemistry.C
+++ src/properties/src/antioch_chemistry.C
@@ -32,6 +32,7 @@
 
 // GRINS
 #include "grins/materials_parsing.h"
+#include "grins/antioch_mixture_builder_base.h"
 
 // Antioch
 #include "antioch/default_filename.h"
@@ -45,38 +46,7 @@ namespace GRINS
                                       const std::string& material )
     : ParameterUser("AntiochChemistry")
   {
-    {
-      std::string warning = "==============================================\n";
-      warning += "WARNING: This AntiochChemistry constructor is DEPREACTED!\n";
-      warning += "         Prefer alternate constructor where parsing\n";
-      warning += "         is done outside this class.\n";
-      warning += "==============================================\n";
-
-      libmesh_warning(warning);
-    }
-    std::vector<std::string> species_list;
-    MaterialsParsing::parse_chemical_species(input,material,species_list);
-
-    bool verbose_antioch_read = input("Materials/"+material+"/GasMixture/Antioch/verbose_read",false);
-
-    std::string species_data_filename = input("Materials/"+material+"/GasMixture/Antioch/species_data", "default" );
-    if( species_data_filename == std::string("default") )
-      species_data_filename = Antioch::DefaultInstallFilename::chemical_mixture();
-
-    std::string vibration_data_filename = input("Materials/"+material+"/GasMixture/Antioch/vibration_data", "default" );
-    if( vibration_data_filename == std::string("default") )
-      vibration_data_filename = Antioch::DefaultInstallFilename::vibrational_data();
-
-    std::string electronic_data_filename = input("Materials/"+material+"/GasMixture/Antioch/electronic_data", "default" );
-    if( electronic_data_filename == std::string("default") )
-      electronic_data_filename = Antioch::DefaultInstallFilename::electronic_data();
-
-    // By default, Antioch is using its ASCII parser. We haven't added more options yet.
-    _antioch_gas.reset( new Antioch::ChemicalMixture<libMesh::Real>( species_list,
-                                                                     verbose_antioch_read,
-                                                                     species_data_filename,
-                                                                     vibration_data_filename,
-                                                                     electronic_data_filename ) );
+    _antioch_gas = AntiochMixtureBuilderBase().build_chem_mix(input,material);
   }
 
   AntiochChemistry::AntiochChemistry
