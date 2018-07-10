12a86dd416084e89a323cc0ebd69f85df35d6a2c
Paul T. Bauman <ptbauman@gmail.com>
2018-07-10 11:59:06 -0400

    Unit testing for build_species_names using XML
    Leverage existing air.xml file. We test a couple of different
    mixtures to make sure it's selecting the correct one from the
    xml file.

--- test/interface/antioch_build_species_names.C
+++ test/interface/antioch_build_species_names.C
@@ -42,6 +42,8 @@ namespace GRINSTesting
     CPPUNIT_TEST_SUITE( AntiochBuildSpeciesTest );
 
     CPPUNIT_TEST( test_ascii );
+    CPPUNIT_TEST( test_xml_air5sp );
+    CPPUNIT_TEST( test_xml_air9sp );
 
     CPPUNIT_TEST_SUITE_END();
 
@@ -52,6 +54,20 @@ namespace GRINSTesting
       std::string inputfile = this->setup_ascii_input("ozone_species_data.dat");
       std::vector<std::string> species_names = this->do_work(inputfile);
       this->check_ascii(species_names);
+    }
+
+    void test_xml_air5sp()
+    {
+      std::string inputfile = this->setup_xml_input("air.xml","air5sp");
+      std::vector<std::string> species_names = this->do_work(inputfile);
+      this->check_xml_air5sp(species_names);
+    }
+
+    void test_xml_air9sp()
+    {
+      std::string inputfile = this->setup_xml_input("air.xml","air9sp_CO2");
+      std::vector<std::string> species_names = this->do_work(inputfile);
+      this->check_xml_air9sp(species_names);
     }
 
   private:
@@ -69,21 +85,28 @@ namespace GRINSTesting
       return species_names;
     }
 
+    std::string setup_xml_input( const std::string & chemical_data_filename, const std::string & mixture )
+    {
+      std::string text = this->basic_input_setup(chemical_data_filename);
+      text += "gas_mixture = '"+mixture+"'\n";
+      return text;
+    }
+
     std::string setup_ascii_input( const std::string & chemical_data_filename )
     {
-      std::string text = this->basic_input_setup();
-      text += "chemical_data = './input_files/"+chemical_data_filename+"'\n";
+      std::string text = this->basic_input_setup(chemical_data_filename);
       text += "species = 'O O2 O3'\n";
 
       return text;
     }
 
-    std::string basic_input_setup()
+    std::string basic_input_setup( const std::string & chemical_data_filename )
     {
       std::string text = "[Materials]\n";
       text +="[./TestMaterial]\n";
       text +="[./GasMixture]\n";
       text +="[./Antioch]\n";
+      text += "chemical_data = './input_files/"+chemical_data_filename+"'\n";
       return text;
     }
 
@@ -93,6 +116,30 @@ namespace GRINSTesting
       CPPUNIT_ASSERT_EQUAL(std::string("O"),species_names[0]);
       CPPUNIT_ASSERT_EQUAL(std::string("O2"),species_names[1]);
       CPPUNIT_ASSERT_EQUAL(std::string("O3"),species_names[2]);
+    }
+
+    void check_xml_air5sp( const std::vector<std::string> & species_names )
+    {
+      CPPUNIT_ASSERT_EQUAL(5,(int)species_names.size());
+      CPPUNIT_ASSERT_EQUAL(std::string("N2"),species_names[0]);
+      CPPUNIT_ASSERT_EQUAL(std::string("O2"),species_names[1]);
+      CPPUNIT_ASSERT_EQUAL(std::string("NO"),species_names[2]);
+      CPPUNIT_ASSERT_EQUAL(std::string("N"),species_names[3]);
+      CPPUNIT_ASSERT_EQUAL(std::string("O"),species_names[4]);
+    }
+
+    void check_xml_air9sp( const std::vector<std::string> & species_names )
+    {
+      CPPUNIT_ASSERT_EQUAL(9,(int)species_names.size());
+      CPPUNIT_ASSERT_EQUAL(std::string("N2"),species_names[0]);
+      CPPUNIT_ASSERT_EQUAL(std::string("O2"),species_names[1]);
+      CPPUNIT_ASSERT_EQUAL(std::string("NO"),species_names[2]);
+      CPPUNIT_ASSERT_EQUAL(std::string("N"),species_names[3]);
+      CPPUNIT_ASSERT_EQUAL(std::string("O"),species_names[4]);
+      CPPUNIT_ASSERT_EQUAL(std::string("CO2"),species_names[5]);
+      CPPUNIT_ASSERT_EQUAL(std::string("CO"),species_names[6]);
+      CPPUNIT_ASSERT_EQUAL(std::string("CN"),species_names[7]);
+      CPPUNIT_ASSERT_EQUAL(std::string("C"),species_names[8]);
     }
 
   };
