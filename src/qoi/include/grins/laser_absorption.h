a589f6e499cb06c15d41dd336876fb3f327158a5
Tim Adowski <tradowsk@buffalo.edu>
2018-12-19 11:40:02 -0500

    Add new LaserAbsorption QoI class

---
+++ src/qoi/include/grins/laser_absorption.h
@@ -0,0 +1,95 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2017 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+
+#ifndef GRINS_LASER_ABSORPTION_H
+#define GRINS_LASER_ABSORPTION_H
+
+// GRINS
+#include "grins/multi_qoi_base.h"
+#include "grins/fem_function_and_derivative_base.h"
+#include "grins/spectroscopic_transmission.h"
+#include "grins/laser_intensity_profile_base.h"
+
+namespace GRINS
+{
+  class LaserAbsorption : public MultiQoIBase
+  {
+  public:
+
+    /*!
+      The LaserAbsorption class is a 2D analogue of SpectroscopicAbsorption that
+      can calculate the total absorbed laser intensity of a two-dimensional
+      laser beam across a given flow field.
+      
+      It uses Gauss quadrature to integrate the initial and final laser intensity profiles.
+      
+      Each quadrature point has a corresponding SpectroscopicTransmission object to calculate the
+      transmitted laser intensity along its respective optical path (i.e. RayfireMesh).
+
+      @param absorb An AbsorptionCoeff object that will be shared by all internal SpectroscopicTransmission classes
+      @param top_origin A point on the mesh boundary at the top of the 2D laser
+      @param centerline_origin A point on the mesh boundary at the centerline of the 2D laser
+      @param bottom_origin A point on the mesh boundary at the bottom of the 2D laser
+      @param theta angle in the xy-plane for the laser path (measured from the positive x-axis)
+      @param n_quadrature_point The number of quadrature points used to integral the intensity profile.
+                                Each quadrature point will have a separate SpectroscopicTransmission class
+      @param intensity_profile A LaserIntensityProfileBase object
+      @param qoi_name The name of the QoI
+    */
+    LaserAbsorption(const std::shared_ptr<FEMFunctionAndDerivativeBase<libMesh::Real>> & absorb,
+                    const libMesh::Point & top_origin, const libMesh::Point & centerline_origin,
+                    const libMesh::Point & bottom_origin,
+                    libMesh::Real theta, unsigned int n_quadrature_points,
+                    std::shared_ptr<LaserIntensityProfileBase> intensity_profile,   
+                    const std::string & qoi_name);
+
+    //! Just call the default copy constructor
+    virtual QoIBase * clone() const;
+
+    virtual void element_qoi( AssemblyContext& context,
+                              const unsigned int qoi_index);
+
+    //! AMR not yet supported
+    virtual void element_qoi_derivative(AssemblyContext & context,
+                                        const unsigned int qoi_index);
+
+    //! Sum _qoi_vals from all processors and then do the gauss quadrature
+    //! integration to find the total absorbed laser intensity
+    virtual void parallel_op( const libMesh::Parallel::Communicator & communicator,
+                              libMesh::Number & sys_qoi,
+                              libMesh::Number & local_qoi );
+
+  private:
+    //! intensity profile object used to get the laser intensity at the quadrature points
+    std::shared_ptr<LaserIntensityProfileBase> _intensity_profile;
+
+    //! gauss quadrature weights cached here for use in parallel_op()
+    std::vector<libMesh::Real> _quadrature_weights;
+
+  };
+
+}
+#endif // GRINS_LASER_ABSORPTION_H
+
---
+++ src/qoi/src/laser_absorption.C
@@ -0,0 +1,151 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// GRINS - General Reacting Incompressible Navier-Stokes
+//
+// Copyright (C) 2014-2017 Paul T. Bauman, Roy H. Stogner
+// Copyright (C) 2010-2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+// GRINS
+#include "grins/laser_absorption.h"
+#include "grins/fem_function_and_derivative_base.h"
+#include "grins/spectroscopic_transmission.h"
+#include "grins/laser_intensity_profile_base.h"
+
+// libMesh
+#include "libmesh/edge_edge2.h"
+#include "libmesh/quadrature_gauss.h"
+
+namespace GRINS
+{
+  LaserAbsorption::LaserAbsorption( const std::shared_ptr<FEMFunctionAndDerivativeBase<libMesh::Real>> & absorb,
+                                    const libMesh::Point & top_origin, const libMesh::Point & centerline_origin,
+                                    const libMesh::Point & bottom_origin,
+                                    libMesh::Real theta, unsigned int n_quadrature_points,
+                                    std::shared_ptr<LaserIntensityProfileBase> intensity_profile,    
+                                    const std::string & qoi_name)
+    : MultiQoIBase(qoi_name),
+      _intensity_profile(intensity_profile)
+  {
+    // create an EDGE2 elem to represent the start of the laser beam
+    std::shared_ptr<libMesh::Elem> elem( new libMesh::Edge2() );
+    elem->set_node(0) = new libMesh::Node(bottom_origin);
+    elem->set_node(1) = new libMesh::Node(top_origin);
+
+    // now use QGauss to identify the quadratures weights and points on this "laser" elem
+    libMesh::Order order = (libMesh::Order)(2*n_quadrature_points - 1);
+    libMesh::QGauss qbase(elem->dim(),order);
+    qbase.init(elem->type(),order);
+
+    std::unique_ptr< libMesh::FEGenericBase<libMesh::Real> > fe = libMesh::FEGenericBase<libMesh::Real>::build(elem->dim(),libMesh::FEType(libMesh::FIRST,libMesh::LAGRANGE));
+    fe->attach_quadrature_rule( &qbase );
+
+    const std::vector<libMesh::Point> & quadrature_xyz = fe->get_xyz();
+
+    fe->reinit(elem.get());
+
+    _quadrature_weights = qbase.get_weights();
+
+    // init the intensity profile object
+    _intensity_profile->init(quadrature_xyz,centerline_origin);
+
+    // create an internal SpectroscopicTramsmission object at each quadrature node
+    std::shared_ptr<RayfireMesh> rayfire;
+    for(unsigned int p = 0; p < quadrature_xyz.size(); ++p)
+      {
+        libMesh::Point origin = quadrature_xyz[p];
+        rayfire.reset( new RayfireMesh(origin,theta) );
+        SpectroscopicTransmission spec(absorb,rayfire,qoi_name,false);
+
+        this->add_qoi(spec);
+      }
+
+  }
+
+  QoIBase * LaserAbsorption::clone() const
+  {
+    return new LaserAbsorption(*this);
+  }
+
+  void LaserAbsorption::element_qoi(AssemblyContext & context, const unsigned int qoi_index)
+  {
+    // perhaps a bit hacky, but since the Context doesn't know about the internal SpectroscopicTransmission
+    // QoIs, we have to use the Context as a way to pass the QoI contributions to the class
+    // and store them internally in _qoi_vals
+    libMesh::Number & qoi = context.get_qois()[qoi_index];
+    for(unsigned int q = 0; q < this->n_qois(); ++q)
+      {
+        qoi = 0.0;
+
+        _qois[q]->element_qoi(context,qoi_index);
+
+        _qoi_vals[q] += qoi;
+      }
+
+    qoi = 0.0;
+
+  }
+
+  void LaserAbsorption::element_qoi_derivative(AssemblyContext & /*context*/, const unsigned int /*qoi_index*/)
+  {
+    // TODO
+    libmesh_not_implemented();
+  }
+
+  void LaserAbsorption::parallel_op(const libMesh::Parallel::Communicator & communicator,
+                                    libMesh::Number & sys_qoi, libMesh::Number & /*local_qoi*/)
+  {
+    // here we don't care about the local_qoi because we keep track of all the
+    // QoI values internally. So we just do our absorption calculation
+    // and set the sys_qoi and _qoi_value at the end
+
+    // we first need to sum the _qoi_vals from all processors
+    // and do the exponential to get the actual transmission value
+    for (unsigned int i = 0; i < this->n_qois(); ++i)
+      _qois[i]->parallel_op(communicator,_qoi_vals[i],_qoi_vals[i]);
+
+    // calculate the initial laser intensity, Io*
+    libMesh::Real initial = 0.0;
+    for (unsigned int i = 0; i < this->n_qois(); ++i)
+      {
+        libMesh::Real intensity = _intensity_profile->intensity(i);
+        libMesh::Real gq_weight = _quadrature_weights[i];
+
+        initial += gq_weight*intensity;
+      }
+
+    // calculate the final laser intensity, If*
+    libMesh::Real final = 0.0;
+    for (unsigned int i = 0; i < this->n_qois(); ++i)
+      {
+        libMesh::Real gq_weight = _quadrature_weights[i];
+        libMesh::Real intensity = _intensity_profile->intensity(i);
+        libMesh::Real qoi_val = _qoi_vals[i];
+
+        final += gq_weight*intensity*qoi_val;
+      }
+
+    // this is the actual QoI value we are looking for: the total absorbed laser intensity
+    libMesh::Real absorbed_laser_intensity = 1.0 - final/initial;
+
+    sys_qoi = absorbed_laser_intensity;
+    QoIBase::_qoi_value = sys_qoi;
+  }
+
+}
