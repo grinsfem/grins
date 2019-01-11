ae94c6508856916c0fc29f429a53fea39eb98d07
Tim Adowski <tradowsk@buffalo.edu>
2019-01-11 14:23:38 -0500

    Add 3D support to LaserAbsorption

--- src/qoi/src/laser_absorption.C
+++ src/qoi/src/laser_absorption.C
@@ -27,13 +27,17 @@
 #include "grins/fem_function_and_derivative_base.h"
 #include "grins/spectroscopic_transmission.h"
 #include "grins/laser_intensity_profile_base.h"
+#include "grins/math_constants.h"
 
 // libMesh
 #include "libmesh/edge_edge2.h"
+#include "libmesh/face_quad9.h"
 #include "libmesh/quadrature_gauss.h"
+#include "libmesh/serial_mesh.h"
 
 namespace GRINS
 {
+  // 2D constructor
   LaserAbsorption::LaserAbsorption( const std::shared_ptr<FEMFunctionAndDerivativeBase<libMesh::Real>> & absorb,
                                     const libMesh::Point & top_origin, const libMesh::Point & centerline_origin,
                                     const libMesh::Point & bottom_origin,
@@ -76,6 +80,95 @@ namespace GRINS
         this->add_qoi(spec);
       }
 
+  }
+
+  // 3D constructor
+  LaserAbsorption::LaserAbsorption(const std::shared_ptr<FEMFunctionAndDerivativeBase<libMesh::Real>> & absorb,
+                    const libMesh::Point & top_origin, const libMesh::Point & centerline_origin,
+                    const libMesh::Point & bottom_origin,
+                    libMesh::Real theta, libMesh::Real phi, unsigned int n_quadrature_points,
+                    std::shared_ptr<LaserIntensityProfileBase> intensity_profile,   
+                    const std::string & qoi_name)
+    : MultiQoIBase(qoi_name),
+      _intensity_profile(intensity_profile)
+  {
+    libMesh::Point P0(centerline_origin);
+    libMesh::Point P1(top_origin);
+    libMesh::Point P2(bottom_origin);
+
+    libMesh::Point P01(P1-P0);
+    libMesh::Point P02(P2-P0);
+    libMesh::Point n = P01.cross(P02);
+
+    libMesh::Point w = n.cross(P01);
+
+    libMesh::Real a = std::acos( (P01*P02) / (P01.norm() * P02.norm()));
+    if ( (a < libMesh::TOLERANCE) || ( std::abs(a - Constants::pi) < libMesh::TOLERANCE) )
+      libmesh_error_msg("Error in LaserAbsorption: top_origin, centerline_origin, and bottom_origin points cannot be colinear");
+
+    libMesh::Real radius = P01.norm();
+
+    // make sure the radius is constant
+    libmesh_assert_less(std::abs(radius-P02.norm()),libMesh::TOLERANCE); 
+
+    libMesh::Point p = P01;
+
+    std::shared_ptr<libMesh::Elem> elem( new libMesh::Quad9() );
+
+    elem->set_node(0) = new libMesh::Node(p);
+    elem->get_node(0)->set_id(0);
+
+    for (unsigned int s = 1; s < 8; ++s)
+      {
+        libMesh::Real angle = Constants::pi/2.0;
+
+        if (s == 4)
+          angle += (Constants::pi/4.0);
+
+        libMesh::Real x1 = std::cos(angle)/radius;
+        libMesh::Real x2 = std::sin(angle)/(w.norm());
+
+        libMesh::Point node = radius*(x1*p + x2*w);
+
+        elem->set_node(s) = new libMesh::Node(node);
+        elem->get_node(s)->set_id(s);
+
+        p = node;
+      }
+
+    elem->set_node(8) = new libMesh::Node(centerline_origin);
+    elem->get_node(8)->set_id(8);
+
+    // need a dummy ID to get through several asserts
+    elem->set_id(0);
+
+    // now use QGauss to identify the quadratures weights and points on this "laser" elem
+    libMesh::Order order = (libMesh::Order)(2*n_quadrature_points - 1);
+    libMesh::QGauss qbase(elem->dim(),order);
+    qbase.init(elem->type(),order);
+
+    std::unique_ptr< libMesh::FEGenericBase<libMesh::Real> > fe = libMesh::FEGenericBase<libMesh::Real>::build(elem->dim(),libMesh::FEType(libMesh::FIRST,libMesh::LAGRANGE));
+    fe->attach_quadrature_rule( &qbase );
+
+    const std::vector<libMesh::Point> & quadrature_xyz = fe->get_xyz();
+
+    fe->reinit(elem.get());
+
+    _quadrature_weights = qbase.get_weights();
+
+    // init the intensity profile object
+    _intensity_profile->init(quadrature_xyz,centerline_origin);
+
+    // create an internal SpectroscopicTramsmission object at each quadrature node
+    std::shared_ptr<RayfireMesh> rayfire;
+    for(unsigned int p = 0; p < quadrature_xyz.size(); ++p)
+      {
+        libMesh::Point origin = quadrature_xyz[p];
+        rayfire.reset( new RayfireMesh(origin,theta,phi) );
+        SpectroscopicTransmission spec(absorb,rayfire,qoi_name,false);
+
+        this->add_qoi(spec);
+      }
   }
 
   QoIBase * LaserAbsorption::clone() const
