6c162b7edaf0a49d282b30619e145b25e1b8ef4a
Paul T. Bauman <ptbauman@gmail.com>
2019-05-02 14:47:20 -0400

    Add second derivatives to MooneyRivlin

--- src/properties/include/grins/mooney_rivlin.h
+++ src/properties/include/grins/mooney_rivlin.h
@@ -55,6 +55,14 @@ namespace GRINS
     libMesh::Real dI2_imp( libMesh::Real I1, libMesh::Real I2, libMesh::Real I3 ) const;
     libMesh::Real dI3_imp( libMesh::Real I1, libMesh::Real I2, libMesh::Real I3 ) const;
 
+    libMesh::Number dI12_imp( libMesh::Number I1, libMesh::Number I2, libMesh::Number I3 ) const;
+    libMesh::Number dI22_imp( libMesh::Number I1, libMesh::Number I2, libMesh::Number I3 ) const;
+    libMesh::Number dI32_imp( libMesh::Number I1, libMesh::Number I2, libMesh::Number I3 ) const;
+
+    libMesh::Number dI1dI2_imp( libMesh::Number I1, libMesh::Number I2, libMesh::Number I3 ) const;
+    libMesh::Number dI1dI3_imp( libMesh::Number I1, libMesh::Number I2, libMesh::Number I3 ) const;
+    libMesh::Number dI2dI3_imp( libMesh::Number I1, libMesh::Number I2, libMesh::Number I3 ) const;
+
     libMesh::Real _C1;
     libMesh::Real _C2;
 
@@ -78,6 +86,47 @@ namespace GRINS
     return -(I1*_C1)/(3*std::pow(I3,4.0/3.0)) - (2*I2*_C2)/(3*std::pow(I3,5.0/3.0));
   }
 
+  inline
+  libMesh::Number MooneyRivlin::dI12_imp
+  ( libMesh::Number /*I1*/, libMesh::Number /*I2*/, libMesh::Number /*I3*/ ) const
+  {
+    return 0.0;
+  }
+
+  inline
+  libMesh::Number MooneyRivlin::dI22_imp
+  ( libMesh::Number /*I1*/, libMesh::Number /*I2*/, libMesh::Number /*I3*/ ) const
+  {
+    return 0.0;
+  }
+
+  inline
+  libMesh::Number MooneyRivlin::dI32_imp
+  ( libMesh::Number I1, libMesh::Number I2, libMesh::Number I3 ) const
+  {
+    return (4*I1*_C1)/(9*std::pow(I3,7.0/3.0)) + (10*I2*_C2)/(9*std::pow(I3,8.0/3.0));
+  }
+
+  inline
+  libMesh::Number MooneyRivlin::dI1dI2_imp
+  ( libMesh::Number /*I1*/, libMesh::Number /*I2*/, libMesh::Number /*I3*/ ) const
+  {
+    return 0.0;
+  }
+
+  inline
+  libMesh::Number MooneyRivlin::dI1dI3_imp
+  ( libMesh::Number /*I1*/, libMesh::Number /*I2*/, libMesh::Number I3 ) const
+  {
+    return -_C1/(3*std::pow(I3,4.0/3.0));
+  }
+
+  inline
+  libMesh::Number MooneyRivlin::dI2dI3_imp
+  ( libMesh::Number /*I1*/, libMesh::Number /*I2*/, libMesh::Number I3 ) const
+  {
+    return -(2*_C2)/(3*std::pow(I3,5.0/3.0));
+  }
 
 } // end namespace GRINS
 
