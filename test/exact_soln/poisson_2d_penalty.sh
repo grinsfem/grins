5da70b52cc8db30cac3c044723b99e45a7424f5d
Roy Stogner <Roy.Stogner@inl.gov>
2026-09-21 10:43:10 -0500

    Add PenaltyDirichlet MMS tests

---
+++ test/exact_soln/poisson_2d_penalty.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+INPUT="${GRINS_TEST_INPUT_DIR}/poisson_2d_penalty.in"
+TESTDATA="./poisson_2d_penalty.xda"
+
+# First run the case with grins.
+${LIBMESH_RUN:-} ${GRINS_BUILDSRC_DIR}/grins \
+  $INPUT \
+  $PETSC_OPTIONS
+
+# Now run the test part to make sure we're getting the correct thing
+export eps=1e-2
+export offset=0
+${LIBMESH_RUN:-} ${GRINS_TEST_DIR}/generic_exact_solution_testing_app \
+  --input $INPUT \
+  vars='u' norms='L2' tol='1.0e-10' \
+  u_L2_error='0' \
+  u_exact_soln="c:=(1+2*$eps)/(1+$eps);x*(c-x)*y*(c-y)+$offset" \
+  test_data=$TESTDATA
+
+# Now remove the test turd
+rm $TESTDATA
